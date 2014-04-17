f57603142f3f8c4e2a5679b4fce004a6255803b4
Taylor Otwell <taylorotwell@gmail.com>
2014-04-17 13:25:13 -0500

    fixing spacing.

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -1,23 +1,23 @@
 class Homestead
   def Homestead.configure(config, settings)
-      # Configure The Box
-      config.vm.box = "laravel/homestead"
-      config.vm.hostname = "homestead"
+    # Configure The Box
+    config.vm.box = "laravel/homestead"
+    config.vm.hostname = "homestead"
 
-      # Configure A Private Network IP
-      config.vm.network :private_network, ip: "192.168.33.10"
+    # Configure A Private Network IP
+    config.vm.network :private_network, ip: "192.168.33.10"
 
-      # Configure A Few VirtualBox Settings
-      config.vm.provider "virtualbox" do |vb|
-        vb.customize ["modifyvm", :id, "--memory", "2048"]
-        vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
-        vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
-      end
+    # Configure A Few VirtualBox Settings
+    config.vm.provider "virtualbox" do |vb|
+      vb.customize ["modifyvm", :id, "--memory", "2048"]
+      vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+      vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+    end
 
-      # Configure Port Forwarding To The Box
-      config.vm.network "forwarded_port", guest: 80, host: 8000
-      config.vm.network "forwarded_port", guest: 3306, host: 33060
-      config.vm.network "forwarded_port", guest: 5432, host: 54320
+    # Configure Port Forwarding To The Box
+    config.vm.network "forwarded_port", guest: 80, host: 8000
+    config.vm.network "forwarded_port", guest: 3306, host: 33060
+    config.vm.network "forwarded_port", guest: 5432, host: 54320
 
     # Generate An SSH Key For The Box
     config.vm.provision "shell" do |s|
