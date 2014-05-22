3b9c54009435b0461d6fad740b2efcc1958d02ea
Dries Vints <dries.vints@gmail.com>
2014-05-22 20:51:16 +0200

    Overwrite if server block exists
    This is to prevent duplicate server blocks in a single file which gives errors when trying to restart nginx.

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -36,7 +36,7 @@ block="server {
 }
 "
 
-echo "$block" >> "/etc/nginx/sites-available/$1"
+echo "$block" > "/etc/nginx/sites-available/$1"
 ln -s "/etc/nginx/sites-available/$1" "/etc/nginx/sites-enabled/$1"
 service nginx restart
 service php5-fpm restart
