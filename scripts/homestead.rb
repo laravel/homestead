c5b0b7fa27ef795bb3523b138aee6a5e474e9a60
Taylor Otwell <taylorotwell@gmail.com>
2014-05-24 21:28:37 -0500

    change default private ip.

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -5,7 +5,7 @@ class Homestead
     config.vm.hostname = "homestead"
 
     # Configure A Private Network IP
-    config.vm.network :private_network, ip: settings["ip"] ||= "192.168.33.10"
+    config.vm.network :private_network, ip: settings["ip"] ||= "192.168.10.10"
 
     # Configure A Few VirtualBox Settings
     config.vm.provider "virtualbox" do |vb|
