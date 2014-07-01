15766211992153f07af8374318d834f5e9e6835a
Anam Hossain <anam@studentservices.com.au>
2014-07-01 11:07:37 +1000

    Speed up sync the shared folders using NFS
    My app was slow on Homestead and i dug up and found NFS can speed up the sync of the shared folders.
    
    https://coderwall.com/p/uaohzg
    
    Works great!.

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -42,7 +42,7 @@ class Homestead
 
     # Register All Of The Configured Shared Folders
     settings["folders"].each do |folder|
-      config.vm.synced_folder folder["map"], folder["to"], type: folder["type"] ||= nil
+      config.vm.synced_folder folder["map"], folder["to"], type: folder["type"] ||= nil, :nfs => true
     end
 
     # Install All The Configured Nginx Sites
