c9ca441ad28ede9eed7221095d6dd03a03e21e16
Taylor Otwell <taylorotwell@gmail.com>
2014-11-14 19:32:44 -0500

    add comment.

--- scripts/after.sh
+++ scripts/after.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-# Add extra things here
+
+# If you would like to do some extra provisioning you may
+# add any commands you wish to this file and they will
+# be run after the Homestead machine is provisioned.
