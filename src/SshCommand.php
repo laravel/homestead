07d1dad7afeea89a56155a0983ec03cffb349d85
Taylor Otwell <taylorotwell@gmail.com>
2014-11-15 16:03:10 -0500

    work on ssh command.

---
+++ src/SshCommand.php
@@ -0,0 +1,35 @@
+<?php namespace Laravel\Homestead;
+
+use Symfony\Component\Process\Process;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SshCommand extends Command {
+
+	/**
+	 * Configure the command options.
+	 *
+	 * @return void
+	 */
+	protected function configure()
+	{
+		$this->setName('ssh')
+                  ->setDescription('Login to the Homestead machine via SSH');
+	}
+
+	/**
+	 * Execute the command.
+	 *
+	 * @param  \Symfony\Component\Console\Input\InputInterface  $input
+	 * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+	 * @return void
+	 */
+	public function execute(InputInterface $input, OutputInterface $output)
+	{
+		chdir(__DIR__.'/../');
+
+		passthru('vagrant ssh');
+	}
+
+}
