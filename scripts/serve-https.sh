9525110c9e88f13be989dd0e5c8cca7ca2cd3e5a
Michael Meyer <m.meyer2k@gmail.com>
2014-11-18 13:22:49 -0600

    Add HTTPS support

---
+++ scripts/serve-https.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+block="server {
+    listen 443;
+    server_name $1;
+    root $2;
+
+    ssl                  on;
+    ssl_certificate      /etc/nginx/ssl/$1.crt;
+    ssl_certificate_key  /etc/nginx/ssl/$1.key;
+
+    ssl_session_timeout  5m;
+
+    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+    ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:ECDHE-RSA-RC4-SHA:ECDHE-ECDSA-RC4-SHA:RC4-SHA:HIGH:!aNULL:!eNULL:!EXPORT:!DES:!3DES:!MD5:!PSK';
+    ssl_prefer_server_ciphers   on;
+
+
+    index index.html index.htm index.php;
+
+    charset utf-8;
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location = /robots.txt  { access_log off; log_not_found off; }
+
+    access_log off;
+    error_log  /var/log/nginx/$1-error.log error;
+
+    error_page 404 /index.php;
+
+    sendfile off;
+
+    location ~ \.php$ {
+        fastcgi_param  HTTPS on;  
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_pass unix:/var/run/php5-fpm.sock;
+        fastcgi_index index.php;
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_intercept_errors on;
+        fastcgi_buffer_size 16k;
+        fastcgi_buffers 4 16k;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+}
+"
+
+mkdir -p /etc/nginx/ssl
+
+openssl req \
+    -new \
+    -newkey rsa:4096 \
+    -days 365 \
+    -nodes \
+    -x509 \
+    -subj "/C=US/ST=XX/L=Springfield/O=Dis/CN=$1" \
+    -keyout /etc/nginx/ssl/$1.key \
+    -out /etc/nginx/ssl/$1.crt \
+    2> /dev/null
+
+echo "$block" > "/etc/nginx/sites-available/$1_https"
+ln -fs "/etc/nginx/sites-available/$1_https" "/etc/nginx/sites-enabled/$1_https"
+service nginx restart
+service php5-fpm restart
