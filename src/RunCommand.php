6ee67fab1c7d1f039c886b6536998b9bbef5f7d8
Jesse O'Brien <jesse@jesse-obrien.ca>
2014-11-25 15:52:23 -0500

    Adding Run command for running ssh commands

---
+++ src/RunCommand.php
@@ -0,0 +1,39 @@
+<?php namespace Laravel\Homestead;
+
+use Symfony\Component\Process\Process;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+class RunCommand extends Command {
+
+	/**
+	 * Configure the command options.
+	 *
+	 * @return void
+	 */
+	protected function configure()
+	{
+		$this
+			->setName('run')
+			->setDescription('Run commands through the Homestead machine via SSH')
+			->addArgument('ssh-command', InputArgument::REQUIRED, 'The command to pass through to the virtual machine.');
+}
+
+/**
+ * Execute the command.
+	 *
+	 * @param  \Symfony\Component\Console\Input\InputInterface  $input
+	 * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+	 * @return void
+	 */
+	public function execute(InputInterface $input, OutputInterface $output)
+	{
+		chdir(__DIR__.'/../');
+
+		$command = $input->getArgument('ssh-command');
+		passthru('vagrant ssh -c "'.$command.'"');
+	}
+
+}
