8c627bbcd59ed52a70b5fb72e788a79c47369851
Drew Hammond <drew@alphagenetica.com>
2014-11-26 06:15:57 -0500

    Fix unescaped variable

--- scripts/serve-hhvm.sh
+++ scripts/serve-hhvm.sh
@@ -27,7 +27,7 @@ block="server {
         fastcgi_split_path_info ^(.+\.php)(/.+)$;
         fastcgi_pass 127.0.0.1:9000;
         fastcgi_index index.php;
-        fastcgi_param SCRIPT_FILENAME \$document_root$fastcgi_script_name;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
         include fastcgi_params;
     }
 
