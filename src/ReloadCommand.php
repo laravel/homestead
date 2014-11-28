c8d8a85a3dc8572f4b9ca5132bce587319115ae3
Shawn Tunney <shawn.tunney@gmail.com>
2014-11-28 11:39:47 -0500

    Adding reload command to restart VM

---
+++ src/ReloadCommand.php
@@ -0,0 +1,45 @@
+<?php namespace Laravel\Homestead;
+
+use Symfony\Component\Process\Process;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ReloadCommand extends Command {
+
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->setName('reload')
+            ->setDescription('Restart the Homestead machine')
+            ->addOption('provision', null, InputOption::VALUE_NONE, 'Run the provisioners on the box.');
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return void
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $command = 'vagrant reload';
+
+        if ($input->getOption('provision'))
+            $command .= ' --provision';
+
+        $process = new Process($command, realpath(__DIR__.'/../'), null, null, null);
+
+        $process->run(function($type, $line) use ($output)
+        {
+            $output->write($line);
+        });
+    }
+
+}
