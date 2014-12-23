69d19dca42c6580209bc7ea3d4e2914f718053c5
Shawn Tunney <shawn.tunney@gmail.com>
2014-12-23 09:57:39 -0500

    Ignore existing MySQL databases instead of re-creating

--- scripts/create-mysql.sh
+++ scripts/create-mysql.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 DB=$1;
-mysql -uhomestead -psecret -e "DROP DATABASE IF EXISTS $DB";
-mysql -uhomestead -psecret -e "CREATE DATABASE $DB";
+mysql -uhomestead -psecret -e "CREATE DATABASE IF NOT EXISTS $DB";
