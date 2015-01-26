ec0304e1a704db068b5667caa76fb1d6422bd637
Martin Lindhe <martin@ubique.se>
2015-01-26 12:23:34 +0100

    init.sh: dont overwrite Homestead.yaml if already exists

--- init.sh
+++ init.sh
@@ -2,7 +2,13 @@
 
 mkdir -p ~/.homestead
 
-cp src/stubs/Homestead.yaml ~/.homestead/Homestead.yaml
+yaml=~/.homestead/Homestead.yaml
+
+if [ -f $yaml ]; then
+	echo "$yaml found, not overwriting it!"
+else
+	cp src/stubs/Homestead.yaml $yaml
+fi;
 cp src/stubs/after.sh ~/.homestead/after.sh
 cp src/stubs/aliases ~/.homestead/aliases
 
