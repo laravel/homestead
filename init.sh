0cef9a50c7dcbda63a239558e6984f352da7839d
Martin Lindhe <martin@ubique.se>
2015-01-26 18:17:34 +0100

    init.sh: also dont overwrite after.sh or aliases if found

--- init.sh
+++ init.sh
@@ -2,14 +2,28 @@
 
 mkdir -p ~/.homestead
 
-yaml=~/.homestead/Homestead.yaml
+homesteadRoot=~/.homestead
+
+yaml=$homesteadRoot/Homestead.yaml
+after=$homesteadRoot/after.sh
+aliases=$homesteadRoot/aliases
 
 if [ -f $yaml ]; then
 	echo "$yaml found, not overwriting it!"
 else
 	cp src/stubs/Homestead.yaml $yaml
 fi;
-cp src/stubs/after.sh ~/.homestead/after.sh
-cp src/stubs/aliases ~/.homestead/aliases
+
+if [ -f $after ]; then
+	echo "$after found, not overwriting it!"
+else
+	cp src/stubs/after.sh $after
+fi;
+
+if [ -f $aliases ]; then
+	echo "$aliases found, not overwriting it!"
+else
+	cp src/stubs/aliases $aliases
+fi;
 
 echo "Homestead initialized!"
