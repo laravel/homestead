a5dfe9092972b4cbfa4053e85c8468299b760a0b
Martin Lindhe <martin@ubique.se>
2015-01-26 18:22:12 +0100

    init.sh: simplify by using 'cp -i', should work on GNU and BSD cp

--- init.sh
+++ init.sh
@@ -4,26 +4,8 @@ mkdir -p ~/.homestead
 
 homesteadRoot=~/.homestead
 
-yaml=$homesteadRoot/Homestead.yaml
-after=$homesteadRoot/after.sh
-aliases=$homesteadRoot/aliases
-
-if [ -f $yaml ]; then
-	echo "$yaml found, not overwriting it!"
-else
-	cp src/stubs/Homestead.yaml $yaml
-fi;
-
-if [ -f $after ]; then
-	echo "$after found, not overwriting it!"
-else
-	cp src/stubs/after.sh $after
-fi;
-
-if [ -f $aliases ]; then
-	echo "$aliases found, not overwriting it!"
-else
-	cp src/stubs/aliases $aliases
-fi;
+cp -i src/stubs/Homestead.yaml $homesteadRoot/Homestead.yaml
+cp -i src/stubs/after.sh $homesteadRoot/after.sh
+cp -i src/stubs/aliases $homesteadRoot/aliases
 
 echo "Homestead initialized!"
