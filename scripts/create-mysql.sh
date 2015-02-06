dbfa1d657cd5ead8dc9ede05d336e02d22dd9147
Austin Kurtz <akurtz@gmail.com>
2015-02-05 23:22:35 -0800

    UTF-8 charset and collation for MySQL
    MySQL's built-in default for new databases is `latin1` / `latin1_swedish_ci`.  Can we make `utf8` / `utf8_unicode_ci` the default for Homestead's MySQL databases?  Specifying it at database creation seems like the simplest solution to me.
    
    We could also/instead modify the Homestead.yaml syntax to specify these.  For example...
    
    ```
    databases:
        - name: homestead
          charset: utf8
          collation: utf8_unicode_ci
    ```
    
    But I'd still argue that UTF-8 should be the default.

--- scripts/create-mysql.sh
+++ scripts/create-mysql.sh
@@ -2,4 +2,4 @@
 
 DB=$1;
 mysql -uhomestead -psecret -e "DROP DATABASE IF EXISTS $DB";
-mysql -uhomestead -psecret -e "CREATE DATABASE $DB";
+mysql -uhomestead -psecret -e "CREATE DATABASE $DB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci";
