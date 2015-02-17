f033f22b82bbba2670eb69cc13b330bf035970e0
jasecara <jase@itsentrix.com>
2015-02-17 07:55:01 -0500

    Update homestead.rb

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -2,7 +2,7 @@ class Homestead
   def Homestead.configure(config, settings)
     # Configure The Box
     config.vm.box = "laravel/homestead"
-    config.vm.hostname = "homestead"
+    config.vm.hostname = "dev.esentrix.com"
 
     # Configure A Private Network IP
     #config.vm.network :private_network, ip: settings["ip"] ||= "192.168.10.10"
