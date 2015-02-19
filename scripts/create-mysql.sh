4d3d2a0333ade8a724048c853902a00d23b99acc
Felix Kiss <felix@felixkiss.com>
2015-02-19 23:32:20 +0100

    Escape database name in MySQL script
    Otherwise it will break, if the database name contains a period.

--- scripts/create-mysql.sh
+++ scripts/create-mysql.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 DB=$1;
-mysql -uhomestead -psecret -e "DROP DATABASE IF EXISTS $DB";
-mysql -uhomestead -psecret -e "CREATE DATABASE $DB";
+mysql -uhomestead -psecret -e "DROP DATABASE IF EXISTS \`$DB\`";
+mysql -uhomestead -psecret -e "CREATE DATABASE \`$DB\`";
