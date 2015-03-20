23407853eb72e89045914fdde4738a8ab757383f
Michael Dyrynda <michael@nucleus.com.au>
2015-03-20 12:52:40 +1030

    Only create the database if it doesn't already exist This prevents trashing existing databases when running homestead --provision

--- scripts/create-mysql.sh
+++ scripts/create-mysql.sh
@@ -2,5 +2,4 @@
 
 DB=$1;
 
-mysql -uhomestead -psecret -e "DROP DATABASE IF EXISTS \`$DB\`";
-mysql -uhomestead -psecret -e "CREATE DATABASE \`$DB\` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci";
+mysql -uhomestead -psecret -e "CREATE DATABASE IF NOT EXISTS \`$DB\` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci";
