2bb6264a4a8b85509054c4180eb58e3b033481cb
Kennedy Tedesco <kennedyt.tw@gmail.com>
2015-07-31 17:24:24 -0300

    Update SSL support
    There is no need of two nginx `server{}` for this purpose.

--- scripts/serve-hhvm.sh
+++ scripts/serve-hhvm.sh
@@ -7,39 +7,7 @@ openssl x509 -req -days 365 -in /etc/nginx/ssl/$1.csr -signkey /etc/nginx/ssl/$1
 
 block="server {
     listen ${3:-80};
-    server_name $1;
-    root \"$2\";
-
-    index index.html index.htm index.php;
-
-    charset utf-8;
-
-    location / {
-        try_files \$uri \$uri/ /index.php?\$query_string;
-    }
-
-    location = /favicon.ico { access_log off; log_not_found off; }
-    location = /robots.txt  { access_log off; log_not_found off; }
-
-    access_log off;
-    error_log  /var/log/nginx/$1-error.log error;
-
-    sendfile off;
-
-    location ~ \.php$ {
-        fastcgi_split_path_info ^(.+\.php)(/.+)$;
-        fastcgi_pass 127.0.0.1:9000;
-        fastcgi_index index.php;
-        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-        include fastcgi_params;
-    }
-
-    location ~ /\.ht {
-        deny all;
-    }
-}
-server {
-    listen ${4:-443};
+    listen ${4:-443} ssl;
     server_name $1;
     root \"$2\";
 
@@ -71,7 +39,6 @@ server {
         deny all;
     }
 
-    ssl on;
     ssl_certificate     /etc/nginx/ssl/$1.crt;
     ssl_certificate_key /etc/nginx/ssl/$1.key;
 }
--- scripts/serve.sh
+++ scripts/serve.sh
@@ -7,6 +7,7 @@ openssl x509 -req -days 365 -in /etc/nginx/ssl/$1.csr -signkey /etc/nginx/ssl/$1
 
 block="server {
     listen ${3:-80};
+    listen ${4:-443} ssl;
     server_name $1;
     root \"$2\";
 
@@ -42,46 +43,7 @@ block="server {
     location ~ /\.ht {
         deny all;
     }
-}
-server {
-    listen ${4:-443};
-    server_name $1;
-    root \"$2\";
 
-    index index.html index.htm index.php;
-
-    charset utf-8;
-
-    location / {
-        try_files \$uri \$uri/ /index.php?\$query_string;
-    }
-
-    location = /favicon.ico { access_log off; log_not_found off; }
-    location = /robots.txt  { access_log off; log_not_found off; }
-
-    access_log off;
-    error_log  /var/log/nginx/$1-ssl-error.log error;
-
-    sendfile off;
-
-    client_max_body_size 100m;
-
-    location ~ \.php$ {
-        fastcgi_split_path_info ^(.+\.php)(/.+)$;
-        fastcgi_pass unix:/var/run/php5-fpm.sock;
-        fastcgi_index index.php;
-        include fastcgi_params;
-        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-        fastcgi_intercept_errors off;
-        fastcgi_buffer_size 16k;
-        fastcgi_buffers 4 16k;
-    }
-
-    location ~ /\.ht {
-        deny all;
-    }
-
-    ssl on;
     ssl_certificate     /etc/nginx/ssl/$1.crt;
     ssl_certificate_key /etc/nginx/ssl/$1.key;
 }
