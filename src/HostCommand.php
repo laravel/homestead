494bce263a77d7262e4deb96be7a33f6d5fef16d
Alexander Mahrt <alexander.mahrt@aboutyou.de>
2015-08-01 22:10:18 +0200

    Reverted composer.lock changed. Used PSR-2 for code style.

--- src/HostCommand.php
+++ src/HostCommand.php
@@ -1,12 +1,12 @@
 <?php
 namespace Laravel\Homestead;
 
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Helper\DialogHelper;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Process\Process;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
 
 class HostCommand extends Command
 {
@@ -29,7 +29,7 @@ class HostCommand extends Command
      * @var DialogHelper A dialog helper for the command.
      */
     protected $dialog;
-    
+
     /**
      * Configure the command options.
      *
@@ -54,13 +54,12 @@ class HostCommand extends Command
     /**
      * Execute the command.
      *
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @param  \Symfony\Component\Console\Input\InputInterface $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface $output
      * @return void
      */
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        /** @var DialogHelper $dialog */
         $this->dialog = $this->getHelper('dialog');
         $this->ip = $input->getArgument('ip');
         $this->hostName = $input->getArgument('hostname');
@@ -68,22 +67,25 @@ class HostCommand extends Command
 
         if ($this->hostFile === false) {
             $output->writeln('We could not find your host file.');
-            return null;
+
+            return;
         }
 
 
         if ($this->dialog->askConfirmation($output,
-            "Mapping '" . $this->ip . "' to '" . $this->hostName . "'. Is that okay? (Y/n) ")) {
+            "Mapping '" . $this->ip . "' to '" . $this->hostName . "'. Is that okay? (Y/n) ")
+        ) {
             $output->writeln('Okay! Will now write to hosts file...');
 
             $command = $this->getCommand();
 
             if ($command === false) {
                 $output->writeln('We could not find the correct command.');
-                return null;
+
+                return;
             }
 
-            $writeProcess = new Process($command, realpath(__DIR__.'/../'),
+            $writeProcess = new Process($command, realpath(__DIR__ . '/../'),
                 array_merge($_SERVER, $_ENV), null, null);
 
             $writeProcess->run();
@@ -100,7 +102,6 @@ class HostCommand extends Command
     protected function getHostsFile()
     {
         if (strpos(strtoupper(PHP_OS), 'WIN') === 0) {
-            // ToDo: Someone should test this! I don't have a windows machine.
             return '%systemroot%\system32\drivers\etc';
         } elseif (strpos(strtoupper(PHP_OS), 'DARWIN') === 0) {
             return '/etc/hosts';
@@ -119,10 +120,9 @@ class HostCommand extends Command
     protected function getCommand()
     {
         if (strpos(strtoupper(PHP_OS), 'WIN') === 0) {
-            // ToDo: Someone should test this! I don't have a windows machine.
             return 'echo "' . $this->ip . ' ' . $this->hostName . '" >> "' . $this->hostFile . '"';
-        } elseif (strpos(strtoupper(PHP_OS), 'DARWIN') === 0
-            || strpos(strtoupper(PHP_OS), 'LINUX') === 0) {
+        } elseif (strpos(strtoupper(PHP_OS), 'DARWIN') === 0 || strpos(strtoupper(PHP_OS), 'LINUX') === 0
+        ) {
             return 'echo "' . $this->ip . ' ' . $this->hostName . '" >> "' . $this->hostFile . '"';
         } else {
             return false;
