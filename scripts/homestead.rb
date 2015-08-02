1c539e9a4a1a1afa95585a1498a6154594ad9614
Vincent Touzet <vincent.touzet@dotsafe.fr>
2015-08-02 12:29:30 +0200

    Add Symfony2 support on serve

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -112,6 +112,9 @@ class Homestead
           if (site.has_key?("hhvm") && site["hhvm"])
             s.path = scriptDir + "/serve-hhvm.sh"
             s.args = [site["map"], site["to"], site["port"] ||= "80", site["ssl"] ||= "443"]
+          elsif (site.has_key?("symfony2") && site["symfony2"])
+            s.path = scriptDir + "/serve-symfony2.sh"
+            s.args = [site["map"], site["to"], site["port"] ||= "80", site["ssl"] ||= "443"]
           else
             s.path = scriptDir + "/serve.sh"
             s.args = [site["map"], site["to"], site["port"] ||= "80", site["ssl"] ||= "443"]
