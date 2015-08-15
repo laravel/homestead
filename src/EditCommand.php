6ab2c7950b152ed4b3e76b086171a2a2c25fbca4
Olivier Parent <olivier.parent@arteveldehs.be>
2015-08-15 17:53:40 +0200

    CS fix reorder use statements from shorter to longer.

--- src/EditCommand.php
+++ src/EditCommand.php
@@ -4,8 +4,8 @@ namespace Laravel\Homestead;
 
 use Symfony\Component\Process\Process;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class EditCommand extends Command
