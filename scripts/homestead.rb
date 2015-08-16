cb421fb9167634cb5879e01b8be4dda6418b1f0f
Allen Eubank <adeubank@gmail.com>
2015-08-15 20:51:35 -0700

    turn off power saving for parallels

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -40,7 +40,7 @@ class Homestead
     # Configure A Few Parallels Settings
     config.vm.provider "parallels" do |v|
       v.update_guest_tools = true
-      v.optimize_power_consumption = true
+      v.optimize_power_consumption = false
       v.memory = settings["memory"] ||= 2048
       v.cpus = settings["cpus"] ||= 1
     end
