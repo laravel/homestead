ec65dd65b6e5e97b301ad275d89c12e6fafe53f7
KwangSeob Jeong <lesstif@gmail.com>
2015-10-19 11:19:02 +0900

    install laravel installer on provision

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -191,6 +191,11 @@ class Homestead
       s.inline = "/usr/local/bin/composer self-update"
     end
 
+    # Install Laravel global installer
+    config.vm.provision "shell" do |s|
+        s.path = scriptDir + "/laravel-installer.sh"
+    end
+
     # Configure Blackfire.io
     if settings.has_key?("blackfire")
       config.vm.provision "shell" do |s|
