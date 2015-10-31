24ac7ff42059ffbb4d3037948b33a64e9a50e6ed
Simonarde Lima <simonarde@gmail.com>
2015-10-31 09:27:37 -0200

    Adding message to the end of script

--- scripts/serve-laravel.sh
+++ scripts/serve-laravel.sh
@@ -56,3 +56,4 @@ echo "$block" > "/etc/nginx/sites-available/$1"
 ln -fs "/etc/nginx/sites-available/$1" "/etc/nginx/sites-enabled/$1"
 service nginx restart
 service php5-fpm restart
+echo "Done! Now $1 should be accessible on your browser :-)"
