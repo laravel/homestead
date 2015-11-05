0bd2ecbd70162d49b7c9dfd8171688458f43c892
Olivier Parent <olivier.parent@arteveldehs.be>
2015-11-05 11:24:47 +0100

    Fixed access denied error on app_dev.php/

--- scripts/serve-symfony2.sh
+++ scripts/serve-symfony2.sh
@@ -31,7 +31,7 @@ block="server {
 
     # DEV
     location ~ ^/(app_dev|config)\.php(/|\$) {
-        fastcgi_split_path_info ^(.+\.php)(/.+)\$;
+        fastcgi_split_path_info ^(.+\.php)(/.*)\$;
         fastcgi_pass unix:/var/run/php5-fpm.sock;
         include fastcgi_params;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
@@ -42,7 +42,7 @@ block="server {
 
     # PROD
     location ~ ^/app\.php(/|$) {
-        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_split_path_info ^(.+\.php)(/.*)$;
         fastcgi_pass unix:/var/run/php5-fpm.sock;
         include fastcgi_params;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
