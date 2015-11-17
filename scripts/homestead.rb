a45ee93104febf3373dd5a66e179e9d77fad1168
Antério vieira da silva <anteriovieira@gmail.com>
2015-11-17 17:15:00 -0200

    Updated parameters for folder sharing

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -109,11 +109,11 @@ class Homestead
         mount_opts = []
 
         if (folder["type"] == "nfs")
-            mount_opts = folder["mount_opts"] ? folder["mount_opts"] : ['actimeo=1']
+            mount_opts = folder["mount_options"] ? folder["mount_options"] : ['actimeo=1']
         end
 
         # For b/w compatibility keep separate 'mount_opts', but merge with options
-        options = (folder["options"] || {}).merge({ mount_opts: mount_opts })
+        options = (folder["options"] || {}).merge({ mount_options: mount_opts })
 
         # Double-splat (**) operator only works with symbol keys, so convert
         options.keys.each{|k| options[k.to_sym] = options.delete(k) }
