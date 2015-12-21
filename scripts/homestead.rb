6bb64c9b8b52a5e954c1ba282be392ae0e34adde
Taylor Otwell <taylorotwell@gmail.com>
2015-12-21 16:36:15 -0600

    fix default box

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -13,7 +13,7 @@ class Homestead
     config.ssh.forward_agent = true
 
     # Configure The Box
-    config.vm.box = settings["box"] ||= "laravel/homestead-7"
+    config.vm.box = settings["box"] ||= "laravel/homestead"
     config.vm.box_version = settings["version"] ||= ">= 0"
     config.vm.hostname = settings["hostname"] ||= "homestead"
 
