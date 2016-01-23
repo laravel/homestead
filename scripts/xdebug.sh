32daa5cc6c40b6036cca57d3cc15643e6c704a52
Justinas Bolys <justinas.bolys@nfq.com>
2016-01-23 20:01:48 +0200

    Also enable xdebug for console apps.

--- scripts/xdebug.sh
+++ scripts/xdebug.sh
@@ -14,6 +14,7 @@ xdebug.cli_color=1
 xdebug.show_local_vars=1
 "
 XDEBUG_INI_PATH="/etc/php/7.0/fpm/conf.d/20-xdebug.ini"
+XDEBUG_CLI_INI_PATH="/etc/php/7.0/cli/conf.d/20-xdebug.ini"
 
 mkdir $TEMP_DIR
 wget $XDEBUG_LINK -O $TEMP_DIR"/xdebug.tgz" -q
@@ -24,8 +25,8 @@ phpize --silent
 ./configure --silent
 make --silent
 sudo cp modules/xdebug.so /usr/lib/php/20151012
-sudo touch $XDEBUG_INI_PATH
 sudo echo "$XDEBUG_LOAD_LINE" > "$XDEBUG_INI_PATH"
+sudo ln -s $XDEBUG_INI_PATH $XDEBUG_CLI_INI_PATH
 
 sudo service nginx restart
 sudo service php7.0-fpm restart
