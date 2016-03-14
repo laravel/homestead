48b47093f604b65c2a81d406db18079eaf225bfb
Lasse Lehtinen <lasse.lehtinen@iki.fi>
2016-03-14 15:34:17 +0200

    Added .my.cnf file to get rid of security warning

--- scripts/create-mysql.sh
+++ scripts/create-mysql.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+cat > ~/.my.cnf << EOF
+[client]
+user = homestead
+password = secret
+host = localhost
+EOF
+
 DB=$1;
 
-mysql -uhomestead -psecret -e "CREATE DATABASE IF NOT EXISTS \`$DB\` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci";
+mysql -e "CREATE DATABASE IF NOT EXISTS \`$DB\` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci";
