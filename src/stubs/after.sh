13510f091559b27fe20502bf9146940dd605c723
Daniel K. Jones <daniel@tomorrowfinance.com.au>
2016-03-17 14:38:44 +1100

    [TASK] Added Apache ANT as part of the development environment for CI/CD purposes

--- src/stubs/after.sh
+++ src/stubs/after.sh
@@ -3,3 +3,6 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
+
+# Install other tools required for local development
+apt-get install -y ant
