560e1d877496ae8b0a7c94c0ded24675ade2cc1d
Dan Richards <dan@shineon.com>
2016-06-27 23:57:21 -0400

    Some commands to run after provisioning for PHP 5.5

--- src/stubs/after.sh
+++ src/stubs/after.sh
@@ -3,3 +3,17 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
+
+# Downgrade to PHP 5.5
+sudo apt-get update
+yes | sudo apt-get install php5.5 php5.5-dev php5.5-mysql php5.5-fpm php5.5-curl
+sudo sed -i 's/php7\.0/php5\.5/g' local.portal.shineon.com
+
+sudo service nginx restart
+sudo service php5.5-fpm restart
+
+echo "Changing to portal.shineon.com Directory"
+cd ~/portal.shineon.com
+composer dump-autoload
+php artisan clear-compiled
+php artisan migrate
