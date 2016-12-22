30af5a1d802ffa19f2d1edeb5e205a22882cc36f
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2016-12-21 23:11:57 -0500

    Solving warning for credentials usage
    Removing the credentials from the mysql command. A copy of the .my.cnf file is created for the vagrant user, to allow easy access to the database.

--- scripts/create-mysql.sh
+++ scripts/create-mysql.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-cat > /home/vagrant/.my.cnf << EOF
+cat > /root/.my.cnf << EOF
 [client]
 user = homestead
 password = secret
 host = localhost
 EOF
 
+cp /root/.my.cnf /home/vagrant/.my.cnf
+
 DB=$1;
 
-mysql --user="root" --password="secret" -e "CREATE DATABASE IF NOT EXISTS \`$DB\` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci";
+mysql -e "CREATE DATABASE IF NOT EXISTS \`$DB\` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci";
