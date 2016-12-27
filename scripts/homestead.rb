6b4c6002b0d549a1abe2029bf10b56e7a2561e9f
Joe Ferguson <joe@joeferguson.me>
2016-12-27 00:15:59 -0600

    🚑 🔥 Add line back that was inadvertently removed in #412

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -33,8 +33,8 @@ class Homestead
       vb.name = settings["name"] ||= "homestead-7"
       vb.customize ["modifyvm", :id, "--memory", settings["memory"] ||= "2048"]
       vb.customize ["modifyvm", :id, "--cpus", settings["cpus"] ||= "1"]
+      vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
       vb.customize ["modifyvm", :id, "--natdnshostresolver1", settings["natdnshostresolver"] ||= "on"]
-      vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
       vb.customize ["modifyvm", :id, "--ostype", "Ubuntu_64"]
       if settings.has_key?("gui") && settings["gui"]
           vb.gui = true
