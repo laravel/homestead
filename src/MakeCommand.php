d3956ef9bc0be477309dbd396ac81d295b4c5c33
Enrico Zimuel <e.zimuel@gmail.com>
2017-02-07 14:31:16 +0100

    Add the host file update + unit test

--- src/MakeCommand.php
+++ src/MakeCommand.php
@@ -9,6 +9,10 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class MakeCommand extends Command
 {
+    const HOSTFILE_LINUX_MAC = '/etc/hosts';
+    const HOSTFILE_WIN       = 'C:\Windows\System32\drivers\etc\hosts';
+    const VM_LOCAL_IP        = '192.168.10.10';
+
     /**
      * The base path of the Laravel installation.
      *
@@ -59,7 +63,7 @@ class MakeCommand extends Command
      * @param  \Symfony\Component\Console\Output\OutputInterface  $output
      * @return void
      */
-    public function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
         if (! file_exists($this->basePath.'/Vagrantfile')) {
             copy(__DIR__.'/stubs/LocalizedVagrantfile', $this->basePath.'/Vagrantfile');
@@ -106,6 +110,9 @@ class MakeCommand extends Command
         }
 
         $this->configurePaths();
+
+        $ip = $input->getOption('ip') ? $input->getOption('ip') : self::VM_LOCAL_IP;
+        $this->updateHostFile($ip, $output);
 
         $output->writeln('Homestead Installed!');
     }
@@ -170,8 +177,33 @@ class MakeCommand extends Command
     protected function updateIpAddress($ip)
     {
         file_put_contents($this->basePath.'/Homestead.yaml', str_replace(
-            'ip: "192.168.10.10"', 'ip: "'.$ip.'"', $this->getHomesteadFile()
+            'ip: "'. self::VM_LOCAL_IP .'"', 'ip: "'.$ip.'"', $this->getHomesteadFile()
         ));
+    }
+
+    /**
+     * Update the host file
+     *
+     * @param string $ip
+     * @param OutputInterface $output
+     * @return void
+     */
+    protected function updateHostFile($ip, OutputInterface $output)
+    {
+        $hostFile = file_exists(self::HOSTFILE_LINUX_MAC) ? self::HOSTFILE_LINUX_MAC : self::HOSTFILE_WIN;
+        $content  = file_get_contents($hostFile);
+        if (!preg_match("/" . $ip . "[\s\S]+homestead.app/", $content)) {
+            if (!is_writable($hostFile)) {
+                if (self::HOSTFILE_WIN === $hostFile) {
+                    $output->writeln(sprintf("Warning: I did not update %s. Please execute as Administrator.", $hostFile));
+                } else {
+                    $output->writeln(sprintf("Warning: I did not update %s. Please execute with SUDO privilege.", $hostFile));
+                }
+                return;
+            }
+            $content .= sprintf("%s\t%s\n", $ip, 'homestead.app');
+            file_put_contents($hostFile, $content);
+        }
     }
 
     /**
