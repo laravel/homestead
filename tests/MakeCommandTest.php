103880051fc45278a1b64295622045aab1d7f7b1
Enrico Zimuel <e.zimuel@gmail.com>
2017-02-08 17:46:15 +0100

    Added unit tests (#445)
    * Added unit tests
    * Changed phpunit version + added @test annotation

---
+++ tests/MakeCommandTest.php
@@ -0,0 +1,47 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase as TestCase;
+use Laravel\Homestead\MakeCommand;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class MakeCommandTest extends TestCase
+{
+    protected static $testFolder;
+
+    public static function setUpBeforeClass()
+    {
+        self::$testFolder = sys_get_temp_dir() . DIRECTORY_SEPARATOR . uniqid("homestead_", true);
+        mkdir(self::$testFolder);
+        chdir(self::$testFolder);
+    }
+
+    public static function tearDownAfterClass()
+    {
+        array_map('unlink', glob(self::$testFolder . DIRECTORY_SEPARATOR . '*'));
+        rmdir(self::$testFolder);
+    }
+
+    /**
+     * @test
+     */
+    public function testConstructor()
+    {
+        $makeCommand = new MakeCommand();
+        $this->assertInstanceOf(MakeCommand::class, $makeCommand);
+    }
+
+    /**
+     * @test
+     */
+    public function testExecuteMake()
+    {
+        $makeCommand = new MakeCommand();
+
+        $tester = new CommandTester($makeCommand);
+        $tester->execute([], []);
+
+        $this->assertContains('Homestead Installed!', $tester->getDisplay());
+        $this->assertEquals(0, $tester->getStatusCode());
+    }
+}
