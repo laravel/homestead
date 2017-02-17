074617e0222612972522dd0d3329080cbc46e7bc
Serjoscha Bassauer @ STORMS|MEDIA <bassauer@storms-media.de>
2017-02-17 01:15:32 +0100

    Check and warn on nonexistant folders (#469)
    * added checking for none existing local dirs
    * changed the error printing like desired by the boss :)

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -132,25 +132,31 @@ class Homestead
     # Register All Of The Configured Shared Folders
     if settings.include? 'folders'
       settings["folders"].each do |folder|
-        mount_opts = []
+        if File.exists? File.expand_path(folder["map"])
+          mount_opts = []
 
-        if (folder["type"] == "nfs")
-            mount_opts = folder["mount_options"] ? folder["mount_options"] : ['actimeo=1', 'nolock']
-        elsif (folder["type"] == "smb")
-            mount_opts = folder["mount_options"] ? folder["mount_options"] : ['vers=3.02', 'mfsymlinks']
-        end
+          if (folder["type"] == "nfs")
+              mount_opts = folder["mount_options"] ? folder["mount_options"] : ['actimeo=1', 'nolock']
+          elsif (folder["type"] == "smb")
+              mount_opts = folder["mount_options"] ? folder["mount_options"] : ['vers=3.02', 'mfsymlinks']
+          end
 
-        # For b/w compatibility keep separate 'mount_opts', but merge with options
-        options = (folder["options"] || {}).merge({ mount_options: mount_opts })
+          # For b/w compatibility keep separate 'mount_opts', but merge with options
+          options = (folder["options"] || {}).merge({ mount_options: mount_opts })
 
-        # Double-splat (**) operator only works with symbol keys, so convert
-        options.keys.each{|k| options[k.to_sym] = options.delete(k) }
+          # Double-splat (**) operator only works with symbol keys, so convert
+          options.keys.each{|k| options[k.to_sym] = options.delete(k) }
 
-        config.vm.synced_folder folder["map"], folder["to"], type: folder["type"] ||= nil, **options
+          config.vm.synced_folder folder["map"], folder["to"], type: folder["type"] ||= nil, **options
 
-        # Bindfs support to fix shared folder (NFS) permission issue on Mac
-        if Vagrant.has_plugin?("vagrant-bindfs")
-          config.bindfs.bind_folder folder["to"], folder["to"]
+          # Bindfs support to fix shared folder (NFS) permission issue on Mac
+          if Vagrant.has_plugin?("vagrant-bindfs")
+            config.bindfs.bind_folder folder["to"], folder["to"]
+          end
+        else
+          config.vm.provision "shell" do |s|
+            s.inline = ">&2 echo \"Unable to mount one of your folders. Please check your folders in Homestead.yaml\""
+          end
         end
       end
     end
