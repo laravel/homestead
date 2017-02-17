dd784084d9fda91e0fb91f8ad11e4fde865a4a93
Roberto Aguilar <raguilar@dojogeek.io>
2017-02-17 15:16:16 -0600

    Use 4 spaces as identation and remove extra lines

--- scripts/serve-apache.sh
+++ scripts/serve-apache.sh
@@ -11,46 +11,46 @@ PATH_CRT="${PATH_SSL}/${1}.crt"
 
 if [ ! -f $PATH_KEY ] || [ ! -f $PATH_CSR ] || [ ! -f $PATH_CRT ]
 then
-  openssl genrsa -out "$PATH_KEY" 2048 2>/dev/null
-  openssl req -new -key "$PATH_KEY" -out "$PATH_CSR" -subj "/CN=$1/O=Vagrant/C=UK" 2>/dev/null
-  openssl x509 -req -days 365 -in "$PATH_CSR" -signkey "$PATH_KEY" -out "$PATH_CRT" 2>/dev/null
+    openssl genrsa -out "$PATH_KEY" 2048 2>/dev/null
+    openssl req -new -key "$PATH_KEY" -out "$PATH_CSR" -subj "/CN=$1/O=Vagrant/C=UK" 2>/dev/null
+    openssl x509 -req -days 365 -in "$PATH_CSR" -signkey "$PATH_KEY" -out "$PATH_CRT" 2>/dev/null
 fi
 
 block="<VirtualHost *:80>
-       # The ServerName directive sets the request scheme, hostname and port that
-       # the server uses to identify itself. This is used when creating
-       # redirection URLs. In the context of virtual hosts, the ServerName
-       # specifies what hostname must appear in the request's Host: header to
-       # match this virtual host. For the default virtual host (this file) this
-       # value is not decisive as it is used as a last resort host regardless.
-       # However, you must set it for any further virtual host explicitly.
-       #ServerName www.example.com
+    # The ServerName directive sets the request scheme, hostname and port that
+    # the server uses to identify itself. This is used when creating
+    # redirection URLs. In the context of virtual hosts, the ServerName
+    # specifies what hostname must appear in the request's Host: header to
+    # match this virtual host. For the default virtual host (this file) this
+    # value is not decisive as it is used as a last resort host regardless.
+    # However, you must set it for any further virtual host explicitly.
+    #ServerName www.example.com
 
-       ServerAdmin webmaster@localhost
-       ServerName $1
-       ServerAlias www.$1
-       DocumentRoot $2
+    ServerAdmin webmaster@localhost
+    ServerName $1
+    ServerAlias www.$1
+    DocumentRoot $2
 
-        <Directory $2>
-            AllowOverride All
-            Require all granted
-        </Directory>
+    <Directory $2>
+        AllowOverride All
+        Require all granted
+    </Directory>
 
-       # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
-       # error, crit, alert, emerg.
-       # It is also possible to configure the loglevel for particular
-       # modules, e.g.
-       #LogLevel info ssl:warn
+    # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+    # error, crit, alert, emerg.
+    # It is also possible to configure the loglevel for particular
+    # modules, e.g.
+    #LogLevel info ssl:warn
 
-       ErrorLog ${APACHE_LOG_DIR}/error.log
-       CustomLog ${APACHE_LOG_DIR}/access.log combined
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
 
-       # For most configuration files from conf-available/, which are
-       # enabled or disabled at a global level, it is possible to
-       # include a line for only one particular virtual host. For example the
-       # following line enables the CGI configuration for this host only
-       # after it has been globally disabled with "a2disconf".
-       #Include conf-available/serve-cgi-bin.conf
+    # For most configuration files from conf-available/, which are
+    # enabled or disabled at a global level, it is possible to
+    # include a line for only one particular virtual host. For example the
+    # following line enables the CGI configuration for this host only
+    # after it has been globally disabled with "a2disconf".
+    #Include conf-available/serve-cgi-bin.conf
 </VirtualHost>
 
 # vim: syntax=apache ts=4 sw=4 sts=4 sr noet
@@ -127,10 +127,8 @@ blockssl="<IfModule mod_ssl.c>
     #SSLCARevocationPath /etc/apache2/ssl.crl/
     #SSLCARevocationFile /etc/apache2/ssl.crl/ca-bundle.crl
 
-
     #SSLVerifyClient require
     #SSLVerifyDepth  10
-
 
     <FilesMatch \"\.(cgi|shtml|phtml|php)$\">
         SSLOptions +StdEnvVars
@@ -153,8 +151,8 @@ blockssl="<IfModule mod_ssl.c>
 
 if [ -f $PATH_KEY ] && [ -f $PATH_CSR ] && [ -f $PATH_CRT ]
 then
-  echo "$blockssl" > "/etc/apache2/sites-available/$1-ssl.conf"
-  ln -fs "/etc/apache2/sites-available/$1-ssl.conf" "/etc/apache2/sites-enabled/$1-ssl.conf"
+    echo "$blockssl" > "/etc/apache2/sites-available/$1-ssl.conf"
+    ln -fs "/etc/apache2/sites-available/$1-ssl.conf" "/etc/apache2/sites-enabled/$1-ssl.conf"
 fi
 
 a2dissite 000-default
