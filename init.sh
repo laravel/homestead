e9661c98b9d6263941b8207f8344d65c99242920
Joe Ferguson <joe@joeferguson.me>
2017-02-24 16:37:38 -0600

    ✨  Add ability to pass argument to init scripts to pick json format (#487)

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-cp -i src/stubs/Homestead.yaml Homestead.yaml
+if [[ -n "$1" ]]; then
+    cp -i src/stubs/Homestead.json Homestead.json
+else
+    cp -i src/stubs/Homestead.yaml Homestead.yaml
+fi
+
 cp -i src/stubs/after.sh after.sh
 cp -i src/stubs/aliases aliases
 
