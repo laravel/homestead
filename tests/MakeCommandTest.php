f63a6216c65c9d8815685f245e53518059ca0f67
Joe Ferguson <joe@joeferguson.me>
2017-02-25 11:47:36 -0600

    ✨ 💡 ✅ Add --json flag for Make Command. Add test coverage for make command

--- tests/MakeCommandTest.php
+++ tests/MakeCommandTest.php
@@ -44,5 +44,45 @@ class MakeCommandTest extends TestCase
 
         $this->assertContains('Homestead Installed!', $tester->getDisplay());
         $this->assertEquals(0, $tester->getStatusCode());
+        $this->assertTrue(file_exists(self::$testFolder.DIRECTORY_SEPARATOR.'Homestead.yaml'));
+        $this->assertTrue(file_exists(self::$testFolder.DIRECTORY_SEPARATOR.'Vagrantfile'));
+
+        // remove files so we can run the command again on a clean folder
+        unlink(self::$testFolder.DIRECTORY_SEPARATOR.'Homestead.yaml');
+        unlink(self::$testFolder.DIRECTORY_SEPARATOR.'Vagrantfile');
+    }
+
+    /**
+     * @test
+     */
+    public function testExecuteMakeWithOptions()
+    {
+        $makeCommand = new MakeCommand();
+
+        $tester = new CommandTester($makeCommand);
+        $tester->execute([
+            '--name' => 'fooname',
+            '--hostname' => 'foohost',
+            '--ip' => '127.0.0.1',
+            '--after' => true,
+            '--aliases' => true,
+            '--example' => true,
+            '--json' => true,
+        ], []);
+
+        $this->assertContains('Homestead Installed!', $tester->getDisplay());
+        $this->assertEquals(0, $tester->getStatusCode());
+        $this->assertTrue(file_exists(self::$testFolder.DIRECTORY_SEPARATOR.'Homestead.json'));
+        $this->assertTrue(file_exists(self::$testFolder.DIRECTORY_SEPARATOR.'Homestead.yaml.example'));
+        $this->assertTrue(file_exists(self::$testFolder.DIRECTORY_SEPARATOR.'Vagrantfile'));
+        $this->assertTrue(file_exists(self::$testFolder.DIRECTORY_SEPARATOR.'aliases'));
+        $this->assertTrue(file_exists(self::$testFolder.DIRECTORY_SEPARATOR.'after.sh'));
+
+        $config = file_get_contents(self::$testFolder.DIRECTORY_SEPARATOR.'Homestead.json');
+        $config = json_decode($config, true);
+
+        $this->assertTrue($config['ip'] === '127.0.0.1');
+        $this->assertTrue($config['name'] === 'fooname');
+        $this->assertTrue($config['hostname'] === 'foohost');
     }
 }
