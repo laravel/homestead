25cf712ab2be3cb93d387dd2e9f25e1c4f7c69a1
Pascal Luxain <pascalln.dev@gmail.com>
2017-03-09 21:37:56 +0100

    Refactor some tests and code Fix #509

---
+++ src/Settings/HomesteadSettingsTrait.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Laravel\Homestead\Settings;
+
+trait HomesteadSettingsTrait
+{
+    /**
+     * Settings attributes.
+     *
+     * @var array
+     */
+    protected $attributes;
+
+    /**
+     * JsonSettings constructor.
+     *
+     * @param  array  $attributes
+     */
+    public function __construct($attributes)
+    {
+        $this->attributes = $attributes;
+    }
+
+    /**
+     * Update the homestead settings.
+     *
+     * @param  array  $attributes
+     * @return static
+     */
+    public function update($attributes)
+    {
+        $this->attributes = array_merge($this->attributes, array_filter($attributes, function ($attribute) {
+            return ! is_null($attribute);
+        }));
+
+        return $this;
+    }
+
+    /**
+     * Update the virtual machine's name.
+     *
+     * @param  string  $name
+     * @return static
+     */
+    public function updateName($name)
+    {
+        $this->update(['name' => $name]);
+
+        return $this;
+    }
+
+    /**
+     * Update the virtual machine's hostname.
+     *
+     * @param  string  $hostname
+     * @return static
+     */
+    public function updateHostname($hostname)
+    {
+        $this->update(['hostname' => $hostname]);
+
+        return $this;
+    }
+
+    /**
+     * Update the virtual machine's IP address.
+     *
+     * @param  string  $ip
+     * @return static
+     */
+    public function updateIpAddress($ip)
+    {
+        $this->update(['ip' => $ip]);
+
+        return $this;
+    }
+
+    /**
+     * Configure the nginx sites.
+     *
+     * @param  string  $projectName
+     * @return static
+     */
+    public function configureSites($projectName, $slugifiedProjectName)
+    {
+        $site = [
+            'map' => "{$projectName}.app",
+            'to' => "/home/vagrant/Code/{$slugifiedProjectName}/public",
+        ];
+
+        $this->update(['sites' => [$site]]);
+
+        return $this;
+    }
+
+    /**
+     * Configure the shared folders.
+     *
+     * @param  string  $projectPath
+     * @param  string  $slugifiedProjectName
+     * @return static
+     */
+    public function configureSharedFolders($projectPath, $slugifiedProjectName)
+    {
+        $folder = [
+            'map' => $projectPath,
+            'to' => "/home/vagrant/Code/{$slugifiedProjectName}",
+        ];
+
+        $this->update(['folders' => [$folder]]);
+
+        return $this;
+    }
+
+    /**
+     * Convert the homestead settings to an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->attributes;
+    }
+
+}
--- src/Settings/JsonSettings.php
+++ src/Settings/JsonSettings.php
@@ -4,22 +4,7 @@ namespace Laravel\Homestead\Settings;
 
 class JsonSettings implements HomesteadSettings
 {
-    /**
-     * Settings attributes.
-     *
-     * @var array
-     */
-    protected $attributes;
-
-    /**
-     * JsonSettings constructor.
-     *
-     * @param  array  $attributes
-     */
-    public function __construct($attributes)
-    {
-        $this->attributes = $attributes;
-    }
+    use HomesteadSettingsTrait;
 
     /**
      * Create an instance from a file.
@@ -43,104 +28,4 @@ class JsonSettings implements HomesteadSettings
         file_put_contents($filename, json_encode($this->attributes, JSON_PRETTY_PRINT));
     }
 
-    /**
-     * Update the homestead settings.
-     *
-     * @param  array  $attributes
-     * @return static
-     */
-    public function update($attributes)
-    {
-        $this->attributes = array_merge($this->attributes, array_filter($attributes, function ($attribute) {
-            return ! is_null($attribute);
-        }));
-
-        return $this;
-    }
-
-    /**
-     * Update the virtual machine's name.
-     *
-     * @param  string  $name
-     * @return static
-     */
-    public function updateName($name)
-    {
-        $this->update(['name' => $name]);
-
-        return $this;
-    }
-
-    /**
-     * Update the virtual machine's hostname.
-     *
-     * @param  string  $hostname
-     * @return static
-     */
-    public function updateHostname($hostname)
-    {
-        $this->update(['hostname' => $hostname]);
-
-        return $this;
-    }
-
-    /**
-     * Update the virtual machine's IP address.
-     *
-     * @param  string  $ip
-     * @return static
-     */
-    public function updateIpAddress($ip)
-    {
-        $this->update(['ip' => $ip]);
-
-        return $this;
-    }
-
-    /**
-     * Configure the nginx sites.
-     *
-     * @param  string  $projectName
-     * @return static
-     */
-    public function configureSites($projectName)
-    {
-        $site = [
-            'map' => "{$projectName}.app",
-            'to' => "/home/vagrant/Code/{$projectName}/public",
-        ];
-
-        $this->update(['sites' => [$site]]);
-
-        return $this;
-    }
-
-    /**
-     * Configure the shared folders.
-     *
-     * @param  string  $projectPath
-     * @param  string  $projectName
-     * @return static
-     */
-    public function configureSharedFolders($projectPath, $projectName)
-    {
-        $folder = [
-            'map' => $projectPath,
-            'to' => "/home/vagrant/Code/{$projectName}",
-        ];
-
-        $this->update(['folders' => [$folder]]);
-
-        return $this;
-    }
-
-    /**
-     * Convert the homestead settings to an array.
-     *
-     * @return array
-     */
-    public function toArray()
-    {
-        return $this->attributes;
-    }
 }
