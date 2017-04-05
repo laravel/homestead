3b0241a824940f0c649d3c55bedd5da159e2735a
Wade Urry <wade@iwader.co.uk>
2017-04-05 23:04:13 +0100

    Add support for SPA site types

---
+++ scripts/serve-spa.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+block="server {
+    listen ${3:-80};
+    listen ${4:-443} ssl http2;
+    server_name $1;
+    root \"$2\";
+
+    index index.html;
+
+    charset utf-8;
+
+    location / {
+        try_files \$uri \$uri/ /index.html;
+    }
+
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location = /robots.txt  { access_log off; log_not_found off; }
+
+    access_log off;
+    error_log  /var/log/nginx/$1-error.log error;
+
+    sendfile off;
+
+    client_max_body_size 100m;
+
+    location ~ /\.ht {
+        deny all;
+    }
+
+    ssl_certificate     /etc/nginx/ssl/$1.crt;
+    ssl_certificate_key /etc/nginx/ssl/$1.key;
+}
+"
+
+echo "$block" > "/etc/nginx/sites-available/$1"
+ln -fs "/etc/nginx/sites-available/$1" "/etc/nginx/sites-enabled/$1"
