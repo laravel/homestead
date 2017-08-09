b038a817d30f905065e0b345d5970ccbace9e9af
Francis Hilaire <Prometee@users.noreply.github.com>
2017-08-09 20:20:16 +0200

    PHP version aware for prod env (#639)

--- scripts/serve-symfony2.sh
+++ scripts/serve-symfony2.sh
@@ -49,7 +49,7 @@ block="server {
     # PROD
     location ~ ^/app\.php(/|$) {
         fastcgi_split_path_info ^(.+\.php)(/.+)$;
-        fastcgi_pass unix:/var/run/php/php7.1-fpm.sock;
+        fastcgi_pass unix:/var/run/php/php$5-fpm.sock;
         include fastcgi_params;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
         $paramsTXT
