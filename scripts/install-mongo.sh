e0a2dce2ade96a0ae48aa9a68aab280e6f458a8b
David Furnes <david@dfurnes.com>
2017-09-14 10:14:48 -0400

    Link mongo extension for CLI.

--- scripts/install-mongo.sh
+++ scripts/install-mongo.sh
@@ -29,6 +29,7 @@ make >/dev/null 2>&1
 sudo make install
 sudo chmod 644 /usr/lib/php/20131226/mongodb.so
 sudo bash -c "echo 'extension=mongodb.so' > /etc/php/5.6/mods-available/mongo.ini"
+sudo ln -s /etc/php/5.6/mods-available/mongo.ini /etc/php/5.6/cli/conf.d/20-mongo.ini
 sudo ln -s /etc/php/5.6/mods-available/mongo.ini /etc/php/5.6/fpm/conf.d/20-mongo.ini
 sudo service php5.6-fpm restart
 
@@ -39,6 +40,7 @@ make >/dev/null 2>&1
 sudo make install
 sudo chmod 644 /usr/lib/php/20151012/mongodb.so
 sudo bash -c "echo 'extension=mongodb.so' > /etc/php/7.0/mods-available/mongo.ini"
+sudo ln -s /etc/php/7.0/mods-available/mongo.ini /etc/php/7.0/cli/conf.d/20-mongo.ini
 sudo ln -s /etc/php/7.0/mods-available/mongo.ini /etc/php/7.0/fpm/conf.d/20-mongo.ini
 sudo service php7.0-fpm restart
 
@@ -49,6 +51,7 @@ make >/dev/null 2>&1
 sudo make install
 sudo chmod 644 /usr/lib/php/20160303/mongodb.so
 sudo bash -c "echo 'extension=mongodb.so' > /etc/php/7.1/mods-available/mongo.ini"
+sudo ln -s /etc/php/7.1/mods-available/mongo.ini /etc/php/7.1/cli/conf.d/20-mongo.ini
 sudo ln -s /etc/php/7.1/mods-available/mongo.ini /etc/php/7.1/fpm/conf.d/20-mongo.ini
 sudo service php7.1-fpm restart
 
