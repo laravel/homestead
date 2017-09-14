de6b5f56210e7ee18919ae3ea9ac860534eda89c
David Furnes <david@dfurnes.com>
2017-09-14 10:42:31 -0400

    Fixes for MongoDB installation bugs on v6.2.0. (#666)
    * Create missing mods-available file.
    
    This was symlinked from a few places, but the file did not actually exist here.
    
    * Link mongo extension for CLI.

--- scripts/install-mongo.sh
+++ scripts/install-mongo.sh
@@ -28,6 +28,8 @@ make clean > /dev/null
 make >/dev/null 2>&1
 sudo make install
 sudo chmod 644 /usr/lib/php/20131226/mongodb.so
+sudo bash -c "echo 'extension=mongodb.so' > /etc/php/5.6/mods-available/mongo.ini"
+sudo ln -s /etc/php/5.6/mods-available/mongo.ini /etc/php/5.6/cli/conf.d/20-mongo.ini
 sudo ln -s /etc/php/5.6/mods-available/mongo.ini /etc/php/5.6/fpm/conf.d/20-mongo.ini
 sudo service php5.6-fpm restart
 
@@ -37,6 +39,8 @@ make clean > /dev/null
 make >/dev/null 2>&1
 sudo make install
 sudo chmod 644 /usr/lib/php/20151012/mongodb.so
+sudo bash -c "echo 'extension=mongodb.so' > /etc/php/7.0/mods-available/mongo.ini"
+sudo ln -s /etc/php/7.0/mods-available/mongo.ini /etc/php/7.0/cli/conf.d/20-mongo.ini
 sudo ln -s /etc/php/7.0/mods-available/mongo.ini /etc/php/7.0/fpm/conf.d/20-mongo.ini
 sudo service php7.0-fpm restart
 
@@ -46,6 +50,8 @@ make clean > /dev/null
 make >/dev/null 2>&1
 sudo make install
 sudo chmod 644 /usr/lib/php/20160303/mongodb.so
+sudo bash -c "echo 'extension=mongodb.so' > /etc/php/7.1/mods-available/mongo.ini"
+sudo ln -s /etc/php/7.1/mods-available/mongo.ini /etc/php/7.1/cli/conf.d/20-mongo.ini
 sudo ln -s /etc/php/7.1/mods-available/mongo.ini /etc/php/7.1/fpm/conf.d/20-mongo.ini
 sudo service php7.1-fpm restart
 
