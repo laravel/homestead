63ca2e31ff37337798e1ce6e57c06b2e2224b3cd
Andrew <browner12@gmail.com>
2017-10-04 10:47:50 -0500

    silently accept the Java license

--- scripts/install-elasticsearch.sh
+++ scripts/install-elasticsearch.sh
@@ -14,6 +14,8 @@ touch /home/vagrant/.elasticsearch
 
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 sudo apt-get -y install oracle-java8-installer
 
 # Install Elasticsearch 5
