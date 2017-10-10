359efcb8ff9ebad7bd6c943acb1feb2423adee01
David Furnes <david@dfurnes.com>
2017-10-10 09:55:56 -0400

    Hide "error" output from adding Mongo key.

--- scripts/install-mongo.sh
+++ scripts/install-mongo.sh
@@ -8,7 +8,7 @@ fi
 
 touch /home/vagrant/.mongo
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6 2>&1
 
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 
