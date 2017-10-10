5e9c14b88b375993500b2c221ebe63e9e47c58e6
David Furnes <david@dfurnes.com>
2017-10-10 11:02:16 -0400

    Start Mongo server earlier in script.

--- scripts/install-mongo.sh
+++ scripts/install-mongo.sh
@@ -16,6 +16,9 @@ sudo apt-get update
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -yq -o Dpkg::Options::="--force-confnew" install mongodb-org autoconf g++ make openssl libssl-dev libcurl4-openssl-dev pkg-config libsasl2-dev php-dev
 
+sudo systemctl enable mongod
+sudo systemctl start mongod
+
 sudo rm -rf /tmp/mongo-php-driver /usr/src/mongo-php-driver
 git clone -c advice.detachedHead=false -q -b '1.2.9' --single-branch https://github.com/mongodb/mongo-php-driver.git /tmp/mongo-php-driver
 sudo mv /tmp/mongo-php-driver /usr/src/mongo-php-driver
@@ -57,8 +60,5 @@ sudo service php7.1-fpm restart
 
 sudo ufw allow 27017
 sudo sed -i "s/bindIp: .*/bindIp: 0.0.0.0/" /etc/mongod.conf
-
-sudo systemctl enable mongod
-sudo systemctl start mongod
 
 mongo admin --eval "db.createUser({user:'homestead',pwd:'secret',roles:['root']})"
