b1f7acaa69b563a234f70763817343d01979a110
Roberto Aguilar <raguilar@dojogeek.io>
2017-10-17 02:10:59 -0500

    Create cron.d directory only if it does not exists

--- scripts/cron-schedule.sh
+++ scripts/cron-schedule.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-mkdir /etc/cron.d 2>/dev/null
+if [ ! -d /etc/cron.d ]; then
+    mkdir /etc/cron.d
+fi
 
 cron="* * * * * vagrant /usr/bin/php $2/../artisan schedule:run >> /dev/null 2>&1"
 
 echo "$cron" > "/etc/cron.d/$1"
+
 service cron restart
