laravel/homestead

commit 34e031513766205ce40021e13db05d5b270faa31

Fix a few more issues with MongoDB provisioning. (#686)
* Hide "error" output from adding Mongo key.

* Create Mongo user in setup, not with each DB.

* Start Mongo server earlier in script.

* Don't restart Mongo after creating each database.

scripts/install-mongo.sh
@@ -8,13 +8,16 @@ fi
 
 touch /home/vagrant/.mongo
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6 2>&1
 
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 
 sudo apt-get update
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -yq -o Dpkg::Options::="--force-confnew" install mongodb-org autoconf g++ make openssl libssl-dev libcurl4-openssl-dev pkg-config libsasl2-dev php-dev
+
+sudo systemctl enable mongod
+sudo systemctl start mongod
 
 sudo rm -rf /tmp/mongo-php-driver /usr/src/mongo-php-driver
 git clone -c advice.detachedHead=false -q -b '1.2.9' --single-branch https://github.com/mongodb/mongo-php-driver.git /tmp/mongo-php-driver
@@ -58,5 +61,4 @@ sudo service php7.1-fpm restart
 sudo ufw allow 27017
 sudo sed -i "s/bindIp: .*/bindIp: 0.0.0.0/" /etc/mongod.conf
 
-sudo systemctl enable mongod
-sudo systemctl start mongod
+mongo admin --eval "db.createUser({user:'homestead',pwd:'secret',roles:['root']})"

scripts/create-mongo.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-mongo admin --eval "db.createUser({user:'homestead',pwd:'secret',roles:['root']})"
 mongo $1 --eval "db.test.insert({name:'db creation'})"
-sudo service mongod restart

commit e9ceafdf03b925a13df0936ce4f6d634aa702556

Modify bind directive before mongod is started
This fixes a non-obvious regression introduced in https://github.com/laravel/homestead/commit/34e031513766205ce40021e13db05d5b270faa31 .

Please see the comments at the above URL for details.

scripts/install-mongo.sh
@@ -16,6 +16,9 @@ sudo apt-get update
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -yq -o Dpkg::Options::="--force-confnew" install mongodb-org autoconf g++ make openssl libssl-dev libcurl4-openssl-dev pkg-config libsasl2-dev php-dev
 
+sudo ufw allow 27017
+sudo sed -i "s/bindIp: .*/bindIp: 0.0.0.0/" /etc/mongod.conf
+
 sudo systemctl enable mongod
 sudo systemctl start mongod
 
@@ -68,8 +71,5 @@ sudo bash -c "echo 'extension=mongodb.so' > /etc/php/7.2/mods-available/mongo.in
 sudo ln -s /etc/php/7.2/mods-available/mongo.ini /etc/php/7.2/cli/conf.d/20-mongo.ini
 sudo ln -s /etc/php/7.2/mods-available/mongo.ini /etc/php/7.2/fpm/conf.d/20-mongo.ini
 sudo service php7.2-fpm restart
-
-sudo ufw allow 27017
-sudo sed -i "s/bindIp: .*/bindIp: 0.0.0.0/" /etc/mongod.conf
 
 mongo admin --eval "db.createUser({user:'homestead',pwd:'secret',roles:['root']})"