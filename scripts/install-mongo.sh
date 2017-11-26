70614cb0129f65e7e7165a91cd344ae6579507bf
Francisco Perez <mail@fperez.me>
2017-11-25 20:11:20 -0800

    updating php mongo driver 1.2.9 to 1.3.3 (#733)

--- scripts/install-mongo.sh
+++ scripts/install-mongo.sh
@@ -23,7 +23,7 @@ sudo systemctl enable mongod
 sudo systemctl start mongod
 
 sudo rm -rf /tmp/mongo-php-driver /usr/src/mongo-php-driver
-git clone -c advice.detachedHead=false -q -b '1.2.9' --single-branch https://github.com/mongodb/mongo-php-driver.git /tmp/mongo-php-driver
+git clone -c advice.detachedHead=false -q -b '1.3.3' --single-branch https://github.com/mongodb/mongo-php-driver.git /tmp/mongo-php-driver
 sudo mv /tmp/mongo-php-driver /usr/src/mongo-php-driver
 cd /usr/src/mongo-php-driver
 git submodule -q update --init
