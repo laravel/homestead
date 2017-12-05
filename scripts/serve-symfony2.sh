26ff75d7aba6a1b1f40ec5acc48d0e733d0a7bcf
Ben Scutt <ben@emailoctopus.com>
2017-12-05 15:05:53 +0000

    Correct the regex used for the fastcgi_split_path_info paramter.
    This is to prevent 403 errors in some circumstances.

--- scripts/serve-symfony2.sh
+++ scripts/serve-symfony2.sh
@@ -36,7 +36,7 @@ block="server {
 
     # DEV
     location ~ ^/(app_dev|app_test|config)\.php(/|\$) {
-        fastcgi_split_path_info ^(.+\.php)(/.+)\$;
+        fastcgi_split_path_info ^(.+\.php)(/.*)\$;
         fastcgi_pass unix:/var/run/php/php$5-fpm.sock;
         include fastcgi_params;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
@@ -49,7 +49,7 @@ block="server {
 
     # PROD
     location ~ ^/app\.php(/|$) {
-        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_split_path_info ^(.+\.php)(/.*)$;
         fastcgi_pass unix:/var/run/php/php$5-fpm.sock;
         include fastcgi_params;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
