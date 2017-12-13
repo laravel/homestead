7d971d29d169f8bf15ad1041718648ec0100c8b4
Charles Peterson <artistan@gmail.com>
2017-12-13 11:29:40 -0600

    cron job variables
    fix for defined variables being passed to cron jobs

--- scripts/cron-schedule.sh
+++ scripts/cron-schedule.sh
@@ -7,6 +7,6 @@ fi
 SITE_DOMAIN=$1
 SITE_PUBLIC_DIRECTORY=$2
 
-cron="* * * * * vagrant /usr/bin/php $SITE_PUBLIC_DIRECTORY/../artisan schedule:run >> /dev/null 2>&1"
+cron="* * * * * vagrant  . /home/vagrant/.profile; /usr/bin/php $SITE_PUBLIC_DIRECTORY/../artisan schedule:run >> /dev/null 2>&1"
 
 echo "$cron" > "/etc/cron.d/$SITE_DOMAIN"
