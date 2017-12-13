35f202a2b7fcbd62eebafe4090adba20132273bb
Charles Peterson <artistan@gmail.com>
2017-12-13 09:41:53 -0600

    Prefix for easy cleanup

--- scripts/cron-schedule.sh
+++ scripts/cron-schedule.sh
@@ -9,4 +9,4 @@ SITE_PUBLIC_DIRECTORY=$2
 
 cron="* * * * * vagrant /usr/bin/php $SITE_PUBLIC_DIRECTORY/../artisan schedule:run >> /dev/null 2>&1"
 
-echo "$cron" > "/etc/cron.d/$SITE_DOMAIN"
+echo "$cron" > "/etc/cron.d/homestead-schedule-$SITE_DOMAIN"
