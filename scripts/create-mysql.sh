5db1fb63ad846292a4233d719dbbec9c1b895de1
Andrey Helldar <helldar@ai-rus.com>
2017-12-21 18:17:46 +0300

    Update default MySQL charset to utf8mb4_unicode_ci (#769)

--- scripts/create-mysql.sh
+++ scripts/create-mysql.sh
@@ -11,4 +11,4 @@ cp /root/.my.cnf /home/vagrant/.my.cnf
 
 DB=$1;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`$DB\` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci";
+mysql -e "CREATE DATABASE IF NOT EXISTS \`$DB\` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8mb4_unicode_ci";
