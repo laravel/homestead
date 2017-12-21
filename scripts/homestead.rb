b2ba5493c5d77154b59f0b748c0a8bfbbf278b7b
Andrey Helldar <helldar@ai-rus.com>
2017-12-21 10:26:07 +0300

    Add `require hirak/prestissimo` composer plugin
    See more: https://github.com/hirak/prestissimo

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -353,7 +353,7 @@ class Homestead
         # Update Composer On Every Provision
         config.vm.provision "shell" do |s|
             s.name = "Update Composer"
-            s.inline = "sudo /usr/local/bin/composer self-update && sudo chown -R vagrant:vagrant /home/vagrant/.composer/"
+            s.inline = "sudo /usr/local/bin/composer self-update && sudo /usr/local/bin/composer global require hirak/prestissimo && sudo chown -R vagrant:vagrant /home/vagrant/.composer/"
             s.privileged = false
         end
 
