c0b7986797f0450e3fad3283cb3ca2a0d6ea2339
Joe Ferguson <joe@joeferguson.me>
2018-02-07 10:16:16 -0600

    🚑 Don't use the Z-Ray Extension unless the user has a site using it.

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -185,6 +185,14 @@ class Homestead
             s.path = scriptDir + "/clear-nginx.sh"
         end
 
+        # Temporary fix to disable Z-Ray by default to be fixed in future base box update
+        config.vm.provision "shell" do |s|
+            s.inline = "rm -rf /usr/lib/php/20170718/zray.so"
+        end
+        config.vm.provision "shell" do |s|
+            s.inline = "rm -rf /etc/php/7.2/fpm/conf.d/zray.ini"
+        end
+
         if settings.include? 'sites'
             settings["sites"].each do |site|
 
@@ -216,6 +224,12 @@ class Homestead
                     if site["zray"] == 'true'
                         config.vm.provision "shell" do |s|
                             s.inline = "ln -sf /opt/zray/gui/public " + site["to"] + "/ZendServer"
+                        end
+                        config.vm.provision "shell" do |s|
+                            s.inline = "ln -sf /opt/zray/lib/zray.so /usr/lib/php/20170718/zray.so"
+                        end
+                        config.vm.provision "shell" do |s|
+                            s.inline = "ln -sf /opt/zray/zray.ini /etc/php/7.2/fpm/conf.d/zray.ini"
                         end
                     else
                         config.vm.provision "shell" do |s|
