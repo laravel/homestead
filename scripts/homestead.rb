66d409615ba6bfbf38e3eac818252a788b550330
Jonas Staudenmeir <mail@jonas-staudenmeir.de>
2018-08-01 00:01:26 +0200

    Fix name/names bug

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -32,7 +32,7 @@ class Homestead
 
     # Configure A Few VirtualBox Settings
     config.vm.provider 'virtualbox' do |vb|
-      vb.name = settings['names'] ||= 'homestead-7'
+      vb.name = settings['name'] ||= 'homestead-7'
       vb.customize ['modifyvm', :id, '--memory', settings['memory'] ||= '2048']
       vb.customize ['modifyvm', :id, '--cpus', settings['cpus'] ||= '1']
       vb.customize ['modifyvm', :id, '--natdnsproxy1', 'on']
@@ -51,7 +51,7 @@ class Homestead
     # Configure A Few VMware Settings
     ['vmware_fusion', 'vmware_workstation'].each do |vmware|
       config.vm.provider vmware do |v|
-        v.vmx['displayName'] = settings['names'] ||= 'homestead-7'
+        v.vmx['displayName'] = settings['name'] ||= 'homestead-7'
         v.vmx['memsize'] = settings['memory'] ||= 2048
         v.vmx['numvcpus'] = settings['cpus'] ||= 1
         v.vmx['guestOS'] = 'ubuntu-64'
@@ -63,7 +63,7 @@ class Homestead
 
     # Configure A Few Parallels Settings
     config.vm.provider 'parallels' do |v|
-      v.name = settings['names'] ||= 'homestead-7'
+      v.name = settings['name'] ||= 'homestead-7'
       v.update_guest_tools = settings['update_parallels_tools'] ||= false
       v.memory = settings['memory'] ||= 2048
       v.cpus = settings['cpus'] ||= 1
