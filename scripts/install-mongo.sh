b2fcd23012d30e12803e7e0817c12db815483033
Francisco Perez <mail@fperez.me>
2018-08-08 11:27:58 -0700

    updating mongodb driver to 1.5.2 (#921)

--- scripts/install-mongo.sh
+++ scripts/install-mongo.sh
@@ -25,7 +25,7 @@ sudo systemctl enable mongod
 sudo systemctl start mongod
 
 sudo rm -rf /tmp/mongo-php-driver /usr/src/mongo-php-driver
-git clone -c advice.detachedHead=false -q -b '1.4.4' --single-branch https://github.com/mongodb/mongo-php-driver.git /tmp/mongo-php-driver
+git clone -c advice.detachedHead=false -q -b '1.5.2' --single-branch https://github.com/mongodb/mongo-php-driver.git /tmp/mongo-php-driver
 sudo mv /tmp/mongo-php-driver /usr/src/mongo-php-driver
 cd /usr/src/mongo-php-driver
 git submodule -q update --init
