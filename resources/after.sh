540303e70f2089c15e83211ec73cdbc82ac6a327
Pierre Galvez <login@pierre-galvez.fr>
2019-01-01 10:51:08 +0100

    Update after.sh

--- resources/after.sh
+++ resources/after.sh
@@ -2,7 +2,7 @@
 
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
-# be run afte the Homestead machine is provisioned.
+# be run after the Homestead machine is provisioned.
 #
 # If you have user-specific configurations you would like
 # to apply, you may also create user-customizations.sh,
