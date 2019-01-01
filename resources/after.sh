0b9a10af5876a49db69c56721ed40977b7a88e7f
Pierre Galvez <login@pierre-galvez.fr>
2019-01-01 10:46:04 +0100

    Fix Bug

--- resources/after.sh
+++ resources/after.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
-# be run after the Homestead machine is provisioned.
+# be run aftethe Homestead machine is provisioned.
 #
 # If you have user-specific configurations you would like
 # to apply, you may also create user-customizations.sh,
