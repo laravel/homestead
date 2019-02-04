ffd3c68d302fb5dbb62c303151fbc196588c147a
Joe Ferguson <joe@joeferguson.me>
2019-02-04 14:54:43 -0600

    🚧 Bad commit DO NOT MERGE

--- src/MakeCommand.php
+++ src/MakeCommand.php
@@ -12,244 +12,244 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class MakeCommand extends Command
 {
-    use GeneratesSlugs;
+use GeneratesSlugs;
 
-    /**
-     * The base path of the Laravel installation.
-     *
-     * @var string
-     */
-    protected $basePath;
+/**
+* The base path of the Laravel installation.
+*
+* @var string
+*/
+protected $basePath;
 
-    /**
-     * The name of the project folder.
-     *
-     * @var string
-     */
-    protected $projectName;
+/**
+* The name of the project folder.
+*
+* @var string
+*/
+protected $projectName;
 
-    /**
-     * Sluggified Project Name.
-     *
-     * @var string
-     */
-    protected $defaultProjectName;
+/**
+* Sluggified Project Name.
+*
+* @var string
+*/
+protected $defaultProjectName;
 
-    /**
-     * Configure the command options.
-     *
-     * @return void
-     */
-    protected function configure()
-    {
-        $this->basePath = getcwd();
-        $this->projectName = basename($this->basePath);
-        $this->defaultProjectName = $this->slug($this->projectName);
+/**
+* Configure the command options.
+*
+* @return void
+*/
+protected function configure()
+{
+$this->basePath = getcwd();
+$this->projectName = basename($this->basePath);
+$this->defaultProjectName = $this->slug($this->projectName);
 
-        $this
-            ->setName('make')
-            ->setDescription('Install Homestead into the current project')
-            ->addOption('name', null, InputOption::VALUE_OPTIONAL, 'The name of the virtual machine.', $this->defaultProjectName)
-            ->addOption('hostname', null, InputOption::VALUE_OPTIONAL, 'The hostname of the virtual machine.', $this->defaultProjectName)
-            ->addOption('ip', null, InputOption::VALUE_OPTIONAL, 'The IP address of the virtual machine.')
-            ->addOption('no-after', null, InputOption::VALUE_NONE, 'Determines if the after.sh file is not created.')
-            ->addOption('no-aliases', null, InputOption::VALUE_NONE, 'Determines if the aliases file is not created.')
-            ->addOption('example', null, InputOption::VALUE_NONE, 'Determines if a Homestead example file is created.')
-            ->addOption('json', null, InputOption::VALUE_NONE, 'Determines if the Homestead settings file will be in json format.');
-    }
+$this
+->setName('make')
+->setDescription('Install Homestead into the current project')
+->addOption('name', null, InputOption::VALUE_OPTIONAL, 'The name of the virtual machine.', $this->defaultProjectName)
+->addOption('hostname', null, InputOption::VALUE_OPTIONAL, 'The hostname of the virtual machine.', $this->defaultProjectName)
+->addOption('ip', null, InputOption::VALUE_OPTIONAL, 'The IP address of the virtual machine.')
+->addOption('no-after', null, InputOption::VALUE_NONE, 'Determines if the after.sh file is not created.')
+->addOption('no-aliases', null, InputOption::VALUE_NONE, 'Determines if the aliases file is not created.')
+->addOption('example', null, InputOption::VALUE_NONE, 'Determines if a Homestead example file is created.')
+->addOption('json', null, InputOption::VALUE_NONE, 'Determines if the Homestead settings file will be in json format.');
+}
 
-    /**
-     * Execute the command.
-     *
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
-     * @return void
-     */
-    public function execute(InputInterface $input, OutputInterface $output)
-    {
-        if (! $this->vagrantfileExists()) {
-            $this->createVagrantfile();
-        }
+/**
+* Execute the command.
+*
+* @param  \Symfony\Component\Console\Input\InputInterface  $input
+* @param  \Symfony\Component\Console\Output\OutputInterface  $output
+* @return void
+*/
+public function execute(InputInterface $input, OutputInterface $output)
+{
+if (! $this->vagrantfileExists()) {
+$this->createVagrantfile();
+}
 
-        if (! $input->getOption('no-aliases') && ! $this->aliasesFileExists()) {
-            $this->createAliasesFile();
-        }
+if (! $input->getOption('no-aliases') && ! $this->aliasesFileExists()) {
+$this->createAliasesFile();
+}
 
-        if (! $input->getOption('no-after') && ! $this->afterShellScriptExists()) {
-            $this->createAfterShellScript();
-        }
+if (! $input->getOption('no-after') && ! $this->afterShellScriptExists()) {
+$this->createAfterShellScript();
+}
 
-        $format = $input->getOption('json') ? 'json' : 'yaml';
+$format = $input->getOption('json') ? 'json' : 'yaml';
 
-        if (! $this->settingsFileExists($format)) {
-            $this->createSettingsFile($format, [
-                'name' => $input->getOption('name'),
-                'hostname' => $input->getOption('hostname'),
-                'ip' => $input->getOption('ip'),
-            ]);
-        }
+if (! $this->settingsFileExists($format)) {
+$this->createSettingsFile($format, [
+'name' => $input->getOption('name'),
+'hostname' => $input->getOption('hostname'),
+'ip' => $input->getOption('ip'),
+]);
+}
 
-        if ($input->getOption('example') && ! $this->exampleSettingsExists($format)) {
-            $this->createExampleSettingsFile($format);
-        }
+if ($input->getOption('example') && ! $this->exampleSettingsExists($format)) {
+$this->createExampleSettingsFile($format);
+}
 
-        $this->checkForDuplicateConfigs($output);
+$this->checkForDuplicateConfigs($output);
 
-        $output->writeln('Homestead Installed!');
-    }
+$output->writeln('Homestead Installed!');
+}
 
-    /**
-     * Determines if Homestead has been installed "per project".
-     *
-     * @return bool
-     */
-    protected function isPerProjectInstallation()
-    {
-        return (bool) preg_match('/vendor\/laravel\/homestead/', __DIR__);
-    }
+/**
+* Determines if Homestead has been installed "per project".
+*
+* @return bool
+*/
+protected function isPerProjectInstallation()
+{
+return (bool) preg_match('/vendor\/laravel\/homestead/', __DIR__);
+}
 
-    /**
-     * Determine if the Vagrantfile exists.
-     *
-     * @return bool
-     */
-    protected function vagrantfileExists()
-    {
-        return file_exists("{$this->basePath}/Vagrantfile");
-    }
+/**
+* Determine if the Vagrantfile exists.
+*
+* @return bool
+*/
+protected function vagrantfileExists()
+{
+return file_exists("{$this->basePath}/Vagrantfile");
+}
 
-    /**
-     * Create a Vagrantfile.
-     *
-     * @return void
-     */
-    protected function createVagrantfile()
-    {
-        copy(__DIR__.'/../resources/localized/Vagrantfile', "{$this->basePath}/Vagrantfile");
-    }
+/**
+* Create a Vagrantfile.
+*
+* @return void
+*/
+protected function createVagrantfile()
+{
+copy(__DIR__.'/../resources/localized/Vagrantfile', "{$this->basePath}/Vagrantfile");
+}
 
-    /**
-     * Determine if the aliases file exists.
-     *
-     * @return bool
-     */
-    protected function aliasesFileExists()
-    {
-        return file_exists("{$this->basePath}/aliases");
-    }
+/**
+* Determine if the aliases file exists.
+*
+* @return bool
+*/
+protected function aliasesFileExists()
+{
+return file_exists("{$this->basePath}/aliases");
+}
 
-    /**
-     * Create aliases file.
-     *
-     * @return void
-     */
-    protected function createAliasesFile()
-    {
-        if ($this->isPerProjectInstallation()) {
-            copy(__DIR__.'/../resources/localized/aliases', "{$this->basePath}/aliases");
-        } else {
-            copy(__DIR__.'/../resources/aliases', "{$this->basePath}/aliases");
-        }
-    }
+/**
+* Create aliases file.
+*
+* @return void
+*/
+protected function createAliasesFile()
+{
+if ($this->isPerProjectInstallation()) {
+copy(__DIR__.'/../resources/localized/aliases', "{$this->basePath}/aliases");
+} else {
+copy(__DIR__.'/../resources/aliases', "{$this->basePath}/aliases");
+}
+}
 
-    /**
-     * Determine if the after shell script exists.
-     *
-     * @return bool
-     */
-    protected function afterShellScriptExists()
-    {
-        return file_exists("{$this->basePath}/after.sh");
-    }
+/**
+* Determine if the after shell script exists.
+*
+* @return bool
+*/
+protected function afterShellScriptExists()
+{
+return file_exists("{$this->basePath}/after.sh");
+}
 
-    /**
-     * Create the after shell script.
-     *
-     * @return void
-     */
-    protected function createAfterShellScript()
-    {
-        copy(__DIR__.'/../resources/after.sh', "{$this->basePath}/after.sh");
-    }
+/**
+* Create the after shell script.
+*
+* @return void
+*/
+protected function createAfterShellScript()
+{
+copy(__DIR__.'/../resources/after.sh', "{$this->basePath}/after.sh");
+}
 
-    /**
-     * Determine if the settings file exists.
-     *
-     * @param  string  $format
-     * @return bool
-     */
-    protected function settingsFileExists($format)
-    {
-        return file_exists("{$this->basePath}/Homestead.{$format}");
-    }
+/**
+* Determine if the settings file exists.
+*
+* @param  string  $format
+* @return bool
+*/
+protected function settingsFileExists($format)
+{
+return file_exists("{$this->basePath}/Homestead.{$format}");
+}
 
-    /**
-     * Create the homestead settings file.
-     *
-     * @param  string  $format
-     * @param  array  $options
-     * @return void
-     */
-    protected function createSettingsFile($format, $options)
-    {
-        $SettingsClass = ($format === 'json') ? JsonSettings::class : YamlSettings::class;
+/**
+* Create the homestead settings file.
+*
+* @param  string  $format
+* @param  array  $options
+* @return void
+*/
+protected function createSettingsFile($format, $options)
+{
+$SettingsClass = ($format === 'json') ? JsonSettings::class : YamlSettings::class;
 
-        $filename = $this->exampleSettingsExists($format) ?
-            "{$this->basePath}/Homestead.{$format}.example" :
-            __DIR__."/../resources/Homestead.{$format}";
+$filename = $this->exampleSettingsExists($format) ?
+"{$this->basePath}/Homestead.{$format}.example" :
+__DIR__."/../resources/Homestead.{$format}";
 
-        $settings = $SettingsClass::fromFile($filename);
+$settings = $SettingsClass::fromFile($filename);
 
-        if (! $this->exampleSettingsExists($format)) {
-            $settings->updateName($options['name'])
-                ->updateHostname($options['hostname']);
-        }
+if (! $this->exampleSettingsExists($format)) {
+$settings->updateName($options['name'])
+->updateHostname($options['hostname']);
+}
 
-        $settings->updateIpAddress($options['ip'])
-            ->configureSites($this->projectName, $this->defaultProjectName)
-            ->configureSharedFolders($this->basePath, $this->defaultProjectName)
-            ->save("{$this->basePath}/Homestead.{$format}");
-    }
+$settings->updateIpAddress($options['ip'])
+->configureSites($this->projectName, $this->defaultProjectName)
+->configureSharedFolders($this->basePath, $this->defaultProjectName)
+->save("{$this->basePath}/Homestead.{$format}");
+}
 
-    /**
-     * Determine if the example settings file exists.
-     *
-     * @param  string  $format
-     * @return bool
-     */
-    protected function exampleSettingsExists($format)
-    {
-        return file_exists("{$this->basePath}/Homestead.{$format}.example");
-    }
+/**
+* Determine if the example settings file exists.
+*
+* @param  string  $format
+* @return bool
+*/
+protected function exampleSettingsExists($format)
+{
+return file_exists("{$this->basePath}/Homestead.{$format}.example");
+}
 
-    /**
-     * Create the homestead settings example file.
-     *
-     * @param  string  $format
-     * @return void
-     */
-    protected function createExampleSettingsFile($format)
-    {
-        copy("{$this->basePath}/Homestead.{$format}", "{$this->basePath}/Homestead.{$format}.example");
-    }
+/**
+* Create the homestead settings example file.
+*
+* @param  string  $format
+* @return void
+*/
+protected function createExampleSettingsFile($format)
+{
+copy("{$this->basePath}/Homestead.{$format}", "{$this->basePath}/Homestead.{$format}.example");
+}
 
-    /**
-     * Checks if JSON and Yaml config files exist, if they do
-     * the user is warned that Yaml will be used before
-     * JSON until Yaml is renamed / removed.
-     *
-     * @param  OutputInterface  $output
-     * @return void
-     */
-    protected function checkForDuplicateConfigs(OutputInterface $output)
-    {
-        if (file_exists("{$this->basePath}/Homestead.yaml") && file_exists("{$this->basePath}/Homestead.json")) {
-            $output->writeln(
-                '<error>WARNING! You have Homestead.yaml AND Homestead.json configuration files</error>'
-            );
-            $output->writeln(
-                '<error>WARNING! Homestead will not use Homestead.json until you rename or delete the Homestead.yaml</error>'
-            );
-        }
-    }
+/**
+* Checks if JSON and Yaml config files exist, if they do
+* the user is warned that Yaml will be used before
+* JSON until Yaml is renamed / removed.
+*
+* @param  OutputInterface  $output
+* @return void
+*/
+protected function checkForDuplicateConfigs(OutputInterface $output)
+{
+if (file_exists("{$this->basePath}/Homestead.yaml") && file_exists("{$this->basePath}/Homestead.json")) {
+$output->writeln(
+'<error>WARNING! You have Homestead.yaml AND Homestead.json configuration files</error>'
+);
+$output->writeln(
+'<error>WARNING! Homestead will not use Homestead.json until you rename or delete the Homestead.yaml</error>'
+);
+}
+}
 }
--- src/Settings/HomesteadSettings.php
+++ src/Settings/HomesteadSettings.php
@@ -4,178 +4,178 @@ namespace Laravel\Homestead\Settings;
 
 abstract class HomesteadSettings
 {
-    /**
-     * Settings attributes.
-     *
-     * @var array
-     */
-    protected $attributes;
+/**
+* Settings attributes.
+*
+* @var array
+*/
+protected $attributes;
 
-    /**
-     * JsonSettings constructor.
-     *
-     * @param  array  $attributes
-     */
-    public function __construct($attributes)
-    {
-        $this->attributes = $attributes;
-    }
+/**
+* JsonSettings constructor.
+*
+* @param  array  $attributes
+*/
+public function __construct($attributes)
+{
+$this->attributes = $attributes;
+}
 
-    /**
-     * Create an instance from a file.
-     *
-     * @param  string  $filename
-     * @return static
-     */
-    abstract public static function fromFile($filename);
+/**
+* Create an instance from a file.
+*
+* @param  string  $filename
+* @return static
+*/
+abstract public static function fromFile($filename);
 
-    /**
-     * Save the homestead settings.
-     *
-     * @param  string  $filename
-     * @return void
-     */
-    abstract public function save($filename);
+/**
+* Save the homestead settings.
+*
+* @param  string  $filename
+* @return void
+*/
+abstract public function save($filename);
 
-    /**
-     * Update the homestead settings.
-     *
-     * @param  array  $attributes
-     * @return static
-     */
-    public function update($attributes)
-    {
-        $this->attributes = array_merge($this->attributes, array_filter($attributes, function ($attribute) {
-            return ! is_null($attribute);
-        }));
+/**
+* Update the homestead settings.
+*
+* @param  array  $attributes
+* @return static
+*/
+public function update($attributes)
+{
+$this->attributes = array_merge($this->attributes, array_filter($attributes, function ($attribute) {
+return ! is_null($attribute);
+}));
 
-        return $this;
-    }
+return $this;
+}
 
-    /**
-     * Update the virtual machine's name.
-     *
-     * @param  string  $name
-     * @return static
-     */
-    public function updateName($name)
-    {
-        $this->update(['name' => $name]);
+/**
+* Update the virtual machine's name.
+*
+* @param  string  $name
+* @return static
+*/
+public function updateName($name)
+{
+$this->update(['name' => $name]);
 
-        return $this;
-    }
+return $this;
+}
 
-    /**
-     * Update the virtual machine's hostname.
-     *
-     * @param  string  $hostname
-     * @return static
-     */
-    public function updateHostname($hostname)
-    {
-        $this->update(['hostname' => $hostname]);
+/**
+* Update the virtual machine's hostname.
+*
+* @param  string  $hostname
+* @return static
+*/
+public function updateHostname($hostname)
+{
+$this->update(['hostname' => $hostname]);
 
-        return $this;
-    }
+return $this;
+}
 
-    /**
-     * Update the virtual machine's IP address.
-     *
-     * @param  string  $ip
-     * @return static
-     */
-    public function updateIpAddress($ip)
-    {
-        $this->update(['ip' => $ip]);
+/**
+* Update the virtual machine's IP address.
+*
+* @param  string  $ip
+* @return static
+*/
+public function updateIpAddress($ip)
+{
+$this->update(['ip' => $ip]);
 
-        return $this;
-    }
+return $this;
+}
 
-    /**
-     * Configure the nginx sites.
-     *
-     * @param  string  $projectName
-     * @param  string  $projectDirectory
-     * @return static
-     */
-    public function configureSites($projectName, $projectDirectory)
-    {
-        $sites = [
-            [
-                'map' => "{$projectName}.test",
-                'to' => "/home/vagrant/{$projectDirectory}/public",
-            ],
-        ];
+/**
+* Configure the nginx sites.
+*
+* @param  string  $projectName
+* @param  string  $projectDirectory
+* @return static
+*/
+public function configureSites($projectName, $projectDirectory)
+{
+$sites = [
+[
+'map' => "{$projectName}.test",
+'to' => "/home/vagrant/{$projectDirectory}/public",
+],
+];
 
-        if (isset($this->attributes['sites']) && ! empty($this->attributes['sites'])) {
-            foreach ($this->attributes['sites'] as $index => $user_site) {
-                if (isset($user_site['map'])) {
-                    $sites[$index]['map'] = $user_site['map'];
-                }
+if (isset($this->attributes['sites']) && ! empty($this->attributes['sites'])) {
+foreach ($this->attributes['sites'] as $index => $user_site) {
+if (isset($user_site['map'])) {
+$sites[$index]['map'] = $user_site['map'];
+}
 
-                if (isset($user_site['to'])) {
-                    $sites[$index]['to'] = $user_site['to'];
-                }
+if (isset($user_site['to'])) {
+$sites[$index]['to'] = $user_site['to'];
+}
 
-                if (isset($user_site['type'])) {
-                    $sites[$index]['type'] = $user_site['type'];
-                }
+if (isset($user_site['type'])) {
+$sites[$index]['type'] = $user_site['type'];
+}
 
-                if (isset($user_site['schedule'])) {
-                    $sites[$index]['schedule'] = $user_site['schedule'];
-                }
+if (isset($user_site['schedule'])) {
+$sites[$index]['schedule'] = $user_site['schedule'];
+}
 
-                if (isset($user_site['php'])) {
-                    $sites[$index]['php'] = $user_site['php'];
-                }
-            }
-        }
+if (isset($user_site['php'])) {
+$sites[$index]['php'] = $user_site['php'];
+}
+}
+}
 
-        $this->update(['sites' => $sites]);
+$this->update(['sites' => $sites]);
 
-        return $this;
-    }
+return $this;
+}
 
-    /**
-     * Configure the shared folders.
-     *
-     * @param  string  $projectPath
-     * @param  string  $projectDirectory
-     * @return static
-     */
-    public function configureSharedFolders($projectPath, $projectDirectory)
-    {
-        $folders = [
-            [
-                'map' => $projectPath,
-                'to' => "/home/vagrant/{$projectDirectory}",
-            ],
-        ];
+/**
+* Configure the shared folders.
+*
+* @param  string  $projectPath
+* @param  string  $projectDirectory
+* @return static
+*/
+public function configureSharedFolders($projectPath, $projectDirectory)
+{
+$folders = [
+[
+'map' => $projectPath,
+'to' => "/home/vagrant/{$projectDirectory}",
+],
+];
 
-        if (isset($this->attributes['folders']) && ! empty($this->attributes['folders'])) {
-            foreach ($this->attributes['folders'] as $index => $user_folder) {
-                if (isset($user_folder['to'])) {
-                    $folders[$index]['to'] = $user_folder['to'];
-                }
+if (isset($this->attributes['folders']) && ! empty($this->attributes['folders'])) {
+foreach ($this->attributes['folders'] as $index => $user_folder) {
+if (isset($user_folder['to'])) {
+$folders[$index]['to'] = $user_folder['to'];
+}
 
-                if (isset($user_folder['type'])) {
-                    $folders[$index]['type'] = $user_folder['type'];
-                }
-            }
-        }
+if (isset($user_folder['type'])) {
+$folders[$index]['type'] = $user_folder['type'];
+}
+}
+}
 
-        $this->update(['folders' => $folders]);
+$this->update(['folders' => $folders]);
 
-        return $this;
-    }
+return $this;
+}
 
-    /**
-     * Convert the homestead settings to an array.
-     *
-     * @return array
-     */
-    public function toArray()
-    {
-        return $this->attributes;
-    }
+/**
+* Convert the homestead settings to an array.
+*
+* @return array
+*/
+public function toArray()
+{
+return $this->attributes;
+}
 }
