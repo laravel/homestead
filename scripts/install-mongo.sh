fb34e46d8308864a4097e35ba48adef9f4eca699
Grigoriy Rozenbaum <bitard3d@gmail.com>
2019-03-06 02:21:35 +0400

    mongodb v4.0.x / bionic instead of xenial (#1066)
    Homestead uses Ubuntu bionic release, this script contains obsolete reference to xenial release of mongodb source
    Changes for proper mongodb source and version 4.0.x

--- scripts/install-mongo.sh
+++ scripts/install-mongo.sh
@@ -10,9 +10,8 @@ fi
 
 touch /home/vagrant/.mongo
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5 2>&1
-
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 2>&1
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 
 sudo apt-get update
 
