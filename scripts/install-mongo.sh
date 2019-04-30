6d8fe452c6f983369597f10b9a0c0d572d4caedd
Ricardo Seriani <ricardo.seriani@gmail.com>
2019-04-30 16:23:58 -0300

    Fix owner of /home/vagrant/.mongo
    Signed-off-by: Ricardo Seriani <ricardo.seriani@gmail.com>

--- scripts/install-mongo.sh
+++ scripts/install-mongo.sh
@@ -9,6 +9,7 @@ then
 fi
 
 touch /home/vagrant/.mongo
+chown -Rf vagrant:vagrant /home/vagrant/.mongo
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 2>&1
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
