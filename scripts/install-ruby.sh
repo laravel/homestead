34bbeaa701b66759fc286485e40b6b00aa1bf42c
Ricardo Seriani <ricardo.seriani@gmail.com>
2019-05-07 16:47:39 -0300

    Point PATH config to ~/.profile, so it's loaded in sudo
    Signed-off-by: Ricardo Seriani <ricardo.seriani@gmail.com>

--- scripts/install-ruby.sh
+++ scripts/install-ruby.sh
@@ -6,11 +6,12 @@ apt-get -y install libssl-dev libyaml-dev libxml2-dev libxslt1-dev libcurl4-open
 libffi-dev
 
 git clone https://github.com/rbenv/rbenv.git /home/vagrant/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.bashrc
-echo 'eval "$(rbenv init -)"' >> /home/vagrant/.bashrc
+cd /home/vagrant/.rbenv && src/configure && make -C src && cd -
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.profile
+echo 'eval "$(rbenv init -)"' >> /home/vagrant/.profile
 
 git clone https://github.com/rbenv/ruby-build.git /home/vagrant/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> /home/vagrant/.bashrc
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> /home/vagrant/.profile
 
 rbenv install 2.6.1
 rbenv global 2.6.1
