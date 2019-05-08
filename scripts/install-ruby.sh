3a0c1251811bf75cba6241987c9b6337dd69a1dd
Ricardo Seriani <ricardo.seriani@gmail.com>
2019-05-07 21:07:03 -0300

    Fix ruby install script (#1130)
    * Remove rbenv from apt install, since it's installed via git
    
    Signed-off-by: Ricardo Seriani <ricardo.seriani@gmail.com>
    
    * Point PATH config to ~/.profile, so it's loaded in sudo
    
    Signed-off-by: Ricardo Seriani <ricardo.seriani@gmail.com>
    
    * Run rbenv and gem as vagrant user
    
    Signed-off-by: Ricardo Seriani <ricardo.seriani@gmail.com>

--- scripts/install-ruby.sh
+++ scripts/install-ruby.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
-# Install Ruby & RVM
+# Install Ruby & rbenv
 
 apt-get -y install libssl-dev libyaml-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common \
-libffi-dev rbenv
+libffi-dev
 
 git clone https://github.com/rbenv/rbenv.git /home/vagrant/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.bashrc
-echo 'eval "$(rbenv init -)"' >> /home/vagrant/.bashrc
+cd /home/vagrant/.rbenv && src/configure && make -C src && cd -
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.profile
+echo 'eval "$(rbenv init -)"' >> /home/vagrant/.profile
 
 git clone https://github.com/rbenv/ruby-build.git /home/vagrant/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> /home/vagrant/.bashrc
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> /home/vagrant/.profile
 
-rbenv install 2.6.1
-rbenv global 2.6.1
-apt-get -y install ruby`ruby -e 'puts RUBY_VERSION[/\d+\.\d+/]'`-dev
-gem install rails -v 5.2.2
-rbenv rehash
 chown -Rf vagrant:vagrant /home/vagrant/.rbenv
+
+# Run as vagrant user
+sudo -i -u vagrant -- rbenv install 2.6.1
+sudo -i -u vagrant -- rbenv global 2.6.1
+sudo -i -u vagrant -- rbenv rehash
+sudo -i -u vagrant -- gem install rails -v 5.2.2
