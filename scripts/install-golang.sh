ad752d2b10be29bff88ca43a16cbdb18d4f2d16b
Ricardo Seriani <ricardo.seriani@gmail.com>
2019-05-07 21:07:19 -0300

    Update golang version to 1.12.5 (#1128)
    Signed-off-by: Ricardo Seriani <ricardo.seriani@gmail.com>

--- scripts/install-golang.sh
+++ scripts/install-golang.sh
@@ -2,7 +2,7 @@
 
 # Install Golang
 
-golangVersion="1.12.4"
+golangVersion="1.12.5"
 wget https://dl.google.com/go/go${golangVersion}.linux-amd64.tar.gz -O golang.tar.gz
 tar -C /usr/local -xzf golang.tar.gz go
 printf "\nPATH=\"/usr/local/go/bin:\$PATH\"\n" | tee -a /home/vagrant/.profile
