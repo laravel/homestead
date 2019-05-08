36ffe67554e270cfadc4db2938d44a00d247df04
Ricardo Seriani <ricardo.seriani@gmail.com>
2019-05-08 12:42:38 -0300

    Change zsh install script to use .zprofile
    Signed-off-by: Ricardo Seriani <ricardo.seriani@gmail.com>

--- scripts/install-ohmyzsh.sh
+++ scripts/install-ohmyzsh.sh
@@ -4,8 +4,9 @@
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
 cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
-printf "\nsource ~/.bash_aliases\n" | tee -a /home/vagrant/.zshrc
-printf "\nsource ~/.profile\n" | tee -a /home/vagrant/.zshrc
+printf "\nemulate sh -c 'source ~/.bash_aliases'\n" | tee -a /home/vagrant/.zprofile
+printf "\nemulate sh -c 'source ~/.profile'\n" | tee -a /home/vagrant/.zprofile
 chown -R vagrant:vagrant /home/vagrant/.oh-my-zsh
 chown vagrant:vagrant /home/vagrant/.zshrc
+chown vagrant:vagrant /home/vagrant/.zprofile
 chsh -s /bin/zsh vagrant
