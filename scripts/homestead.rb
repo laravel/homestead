294d7ebb6cae561130b390776aeb6fdf70928e72
Ricardo Seriani <ricardo.seriani@gmail.com>
2019-05-25 08:27:08 -0300

    Change influxdb install to use features
    Signed-off-by: Ricardo Seriani <ricardo.seriani@gmail.com>

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -375,13 +375,6 @@ class Homestead
       s.inline = 'sudo service nginx restart; sudo service php7.1-fpm restart; sudo service php7.2-fpm restart; sudo service php7.3-fpm restart;'
     end
 
-    # Install InfluxDB if Necessary
-    if settings.has_key?('influxdb') && settings['influxdb']
-        config.vm.provision 'shell' do |s|
-            s.path = script_dir + '/install-influxdb.sh'
-        end
-    end
-
     # Install MariaDB If Necessary
     if settings.has_key?('mariadb') && settings['mariadb']
       config.vm.provision 'shell' do |s|
