7d48b686ae71a68b5615e26e0017d09d448917e5
Ricardo Seriani <ricardo.seriani@gmail.com>
2019-05-25 08:43:43 -0300

    Change minio install to use features
    Signed-off-by: Ricardo Seriani <ricardo.seriani@gmail.com>

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -375,13 +375,6 @@ class Homestead
       s.inline = 'sudo service nginx restart; sudo service php7.1-fpm restart; sudo service php7.2-fpm restart; sudo service php7.3-fpm restart;'
     end
 
-    # Install Minio If Necessary
-    if settings.has_key?('minio') && settings['minio']
-      config.vm.provision 'shell' do |s|
-        s.path = script_dir + '/install-minio.sh'
-      end
-    end
-
     # Install MongoDB If Necessary
     if settings.has_key?('mongodb') && settings['mongodb']
       config.vm.provision 'shell' do |s|
