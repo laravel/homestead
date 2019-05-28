2ddeef558fb7a2ef5a5417459394402a255076cd
stefan <stefnats@users.noreply.github.com>
2019-05-28 17:52:09 +0200

    Implement Apache Solr as optional feature (#1169)
    * Implement Apache Solr as optional feature
    
    * Set script permission to be executable (Solr Feature)
    
    * Use apt instead of apt-get

---
+++ scripts/features/solr.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Install Java Runtime Enviroment
+sudo apt update
+sudo apt install default-jre php-solr -y
+
+# Install Solr 7.7.1
+wget -q http://archive.apache.org/dist/lucene/solr/7.7.1/solr-7.7.1.tgz
+tar xzf solr-7.7.1.tgz solr-7.7.1/bin/install_solr_service.sh --strip-components=2
+sudo bash ./install_solr_service.sh solr-7.7.1.tgz 
+rm solr-7.7.1.tgz install_solr_service.sh
+
+# Install Homestead Core
+
+sudo su -c "/opt/solr/bin/solr create -c homestead" solr
