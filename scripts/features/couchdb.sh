3e5848ba9755967e0f924349b87959635ac78174
Joe Ferguson <joe@joeferguson.me>
2019-05-29 14:17:26 -0500

    Fix Graphana install & CouchDB install

--- scripts/features/couchdb.sh
+++ scripts/features/couchdb.sh
@@ -2,27 +2,32 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-if [ -f /home/vagrant/.homestead-features/couchdb ]
+if [ -f /home/vagrant/.couch ]
 then
     echo "CouchDB already installed."
     exit 0
 fi
 
-touch /home/vagrant/.homestead-features/couchdb
-chown -Rf vagrant:vagrant /home/vagrant/.homestead-features/couchdb
+touch /home/vagrant/.couch
+chown -Rf vagrant:vagrant /home/vagrant/.couch
 
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:couchdb/stable
+echo "deb https://apache.bintray.com/couchdb-deb bionic main" \
+    | sudo tee -a /etc/apt/sources.list
 
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
 sudo apt-get update
-sudo apt-get install -y couchdb
+echo "couchdb couchdb/mode select standalone
+couchdb couchdb/mode seen true
+couchdb couchdb/bindaddress string 127.0.0.1
+couchdb couchdb/bindaddress seen true" | debconf-set-selections
+DEBIAN_FRONTEND=noninteractive apt-get install -y couchdb
 
-sudo chown -R couchdb:couchdb /usr/bin/couchdb /etc/couchdb /usr/share/couchdb
-sudo chmod -R 0770 /usr/bin/couchdb /etc/couchdb /usr/share/couchdb
+sudo chown -R couchdb:couchdb /etc/couchdb
+sudo chmod -R 0770 /etc/couchdb
 
-sudo sed -i "s/;bind_address =.*/bind_address = 0.0.0.0/" /etc/couchdb/local.ini
+sudo sed -i "s/;bind_address =.*/bind_address = 0.0.0.0/" /opt/couchdb/etc/local.ini
 
-sudo systemctl restart couchdb
+sudo service couchdb restart
 
 sudo service nginx restart
 
