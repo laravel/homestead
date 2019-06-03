cceed129204e7385b939c28ca0d1cd38da486cd8
Jonas Staudenmeir <mail@jonas-staudenmeir.de>
2019-06-03 20:46:11 +0200

    Fix webdriver installation

--- scripts/install-webdriver.sh
+++ scripts/install-webdriver.sh
@@ -11,5 +11,6 @@ chown -Rf vagrant:vagrant /home/vagrant/.webdriverutils
 
 # Install The Chrome Web Driver & Dusk Utilities
 
+apt-get update
 apt-get -y install libxpm4 libxrender1 libgtk2.0-0 libnss3 libgconf-2-4 chromium-browser xvfb gtk2-engines-pixbuf \
 xfonts-cyrillic xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable imagemagick x11-apps
