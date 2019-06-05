aa705c5da7abd1b1fe617549cecd3cd75f8f8615
Stefan Neuhaus <neuhaus@clousale.com>
2019-06-05 08:32:53 +0200

    Implement Gearman Job Server as optional feature

---
+++ scripts/features/gearman.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ -f /home/vagrant/.homestead-features/gearman ]
+then
+    echo "gearman already installed."
+    exit 0
+fi
+
+touch /home/vagrant/.homestead-features/gearman
+
+# Install Gearman Job Server and PHP Extension
+sudo DEBIAN_FRONTEND=noninteractive apt install gearman-job-server php-gearman -y
+
+# Listen on 0.0.0.0
+sudo sed -i 's/localhost/0.0.0.0/g' /etc/default/gearman-job-server
+sudo service gearman-job-server restart
