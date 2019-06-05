f3e09e2f4670f4931ab0e42610f020110b8f643a
stefan <stefnats@users.noreply.github.com>
2019-06-05 21:50:38 +0200

    Implement Gearman Job Server as optional feature (#1180)
    * Implement Apache Solr as optional feature
    
    * Set script permission to be executable (Solr Feature)
    
    * Use apt instead of apt-get
    
    * Implement Gearman Job Server as optional feature
    
    * Use apt-get for installing gearman job server

---
+++ scripts/features/gearman.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ -f /home/vagrant/.homestead-features/gearman ]
+then
+    echo "gearman already installed."
+    exit 0
+fi
+
+touch /home/vagrant/.homestead-features/gearman
+
+# Install Gearman Job Server and PHP Extension
+sudo apt-get update
+sudo apt-get install gearman-job-server php-gearman -y
+
+# Listen on 0.0.0.0
+sudo sed -i 's/localhost/0.0.0.0/g' /etc/default/gearman-job-server
+sudo service gearman-job-server restart
