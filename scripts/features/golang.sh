6c8baeafc2bbd60c1c2dc0f525c5938577e52983
Ricardo Seriani <ricardo.seriani@gmail.com>
2019-06-12 09:31:19 -0300

    Update golang version to 1.12.6 (#1183)
    Signed-off-by: Ricardo Seriani <ricardo.seriani@gmail.com>

--- scripts/features/golang.sh
+++ scripts/features/golang.sh
@@ -10,7 +10,7 @@ touch /home/vagrant/.homestead-features/go
 
 # Install Golang
 
-golangVersion="1.12.5"
+golangVersion="1.12.6"
 wget https://dl.google.com/go/go${golangVersion}.linux-amd64.tar.gz -O golang.tar.gz
 tar -C /usr/local -xzf golang.tar.gz go
 printf "\nPATH=\"/usr/local/go/bin:\$PATH\"\n" | tee -a /home/vagrant/.profile
