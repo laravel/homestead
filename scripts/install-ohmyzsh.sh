781f9f86e4d019d0f49bfb8d68b9c2c7056c5f11
Matthew Oliveira <m4olivei@gmail.com>
2019-06-20 14:31:36 -0400

    Issue #1191 Change owner of the flag file for oh my zsh installation

--- scripts/install-ohmyzsh.sh
+++ scripts/install-ohmyzsh.sh
@@ -9,6 +9,7 @@ then
 fi
 
 touch /home/vagrant/.ohmyzsh
+chown -Rf vagrant:vagrant /home/vagrant/.ohmyzsh
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
 cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
