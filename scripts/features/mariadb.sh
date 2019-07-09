2f55716dcbda0c61fc28482f100d89fee4ebe8f8
Pavel Omelchenko <p.Omelchenko@gmail.com>
2019-07-09 17:41:29 +0300

    Use mysql_upgrade for upgrade tables

--- scripts/features/mariadb.sh
+++ scripts/features/mariadb.sh
@@ -64,4 +64,7 @@ mysql --user="root" -e "GRANT ALL ON *.* TO 'homestead'@'%' IDENTIFIED BY 'secre
 mysql --user="root" -e "FLUSH PRIVILEGES;"
 service mysql restart
 
+mysql_upgrade --user="root" --password="secret" --verbose --force
+service mysql restart
+
 unset MYSQL_PWD
