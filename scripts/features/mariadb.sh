65d07ba0a0b5f59b698bd1fa987dc4ca32eaac28
Pavel Omelchenko <p.omelchenko@gmail.com>
2019-07-09 22:54:59 +0300

    Add mysql upgrade for mariadb (#1231)
    * Change configs for remote access and ignoring db dirs
    
    * Use MYSQL_PWD for password
    
    * Create user if not exists
    
    * Use mysql_upgrade for upgrade tables
    
    * Remove password from command properties

--- scripts/features/mariadb.sh
+++ scripts/features/mariadb.sh
@@ -46,15 +46,25 @@ debconf-set-selections <<< "mariadb-server mysql-server/root_password_again pass
 
 apt-get install -y mariadb-server
 
-# Configure Maria Remote Access
+# Configure Maria Remote Access and ignore db dirs
+cat > /etc/mysql/conf.d/mysql.cnf << EOF
+[mysqld]
+bind-address = 0.0.0.0
+ignore-db-dir = lost+found
+EOF
 
-sed -i '/^bind-address/s/bind-address.*=.*/bind-address = 0.0.0.0/' /etc/mysql/my.cnf
+export MYSQL_PWD=secret
 
-mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO root@'0.0.0.0' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
+mysql --user="root" -e "GRANT ALL ON *.* TO root@'0.0.0.0' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
 service mysql restart
 
-mysql --user="root" --password="secret" -e "CREATE USER 'homestead'@'0.0.0.0' IDENTIFIED BY 'secret';"
-mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'homestead'@'0.0.0.0' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
-mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'homestead'@'%' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
-mysql --user="root" --password="secret" -e "FLUSH PRIVILEGES;"
+mysql --user="root" -e "CREATE USER IF NOT EXISTS 'homestead'@'0.0.0.0' IDENTIFIED BY 'secret';"
+mysql --user="root" -e "GRANT ALL ON *.* TO 'homestead'@'0.0.0.0' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
+mysql --user="root" -e "GRANT ALL ON *.* TO 'homestead'@'%' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
+mysql --user="root" -e "FLUSH PRIVILEGES;"
 service mysql restart
+
+mysql_upgrade --user="root" --verbose --force
+service mysql restart
+
+unset MYSQL_PWD
