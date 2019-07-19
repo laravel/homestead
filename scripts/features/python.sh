54b014185700cdafe25f935aa1b314a743630bb5
Joe Ferguson <joe@joeferguson.me>
2019-07-19 09:27:44 -0500

    Run apt-get update before installing python feature

--- scripts/features/python.sh
+++ scripts/features/python.sh
@@ -12,7 +12,7 @@ touch /home/vagrant/.homestead-features/pythontools
 chown -Rf vagrant:vagrant /home/vagrant/.homestead-features
 
 # Install Python
-
-apt install -y python3-pip build-essential libssl-dev libffi-dev python3-dev python3-venv python-django
+apt-get update
+apt-get install -y python3-pip build-essential libssl-dev libffi-dev python3-dev python3-venv python-django
 sudo -H -u vagrant bash -c 'pip3 install django'
 sudo -H -u vagrant bash -c 'pip3 install numpy'
