93628a6cd82a68e8952a477a196032ecfd340e1e
Tristan <tristan@macinnis.net>
2019-07-21 06:30:39 -0700

    Correct variable index in response to zray removal (#1247)
    * Shift array values in reaction to new xhgui option being added
    
    * shift variable index in response to zray removal

--- scripts/site-types/pimcore.sh
+++ scripts/site-types/pimcore.sh
@@ -5,8 +5,8 @@ apt-get update
 apt-get install -y php"$5"-bz2
 
 declare -A params=$6       # Create an associative array
-declare -A headers=${10}   # Create an associative array
-declare -A rewrites=${11}  # Create an associative array
+declare -A headers=${9}    # Create an associative array
+declare -A rewrites=${10}  # Create an associative array
 paramsTXT=""
 if [ -n "$6" ]; then
    for element in "${!params[@]}"
@@ -16,7 +16,7 @@ if [ -n "$6" ]; then
    done
 fi
 headersTXT=""
-if [ -n "${10}" ]; then
+if [ -n "${9}" ]; then
    for element in "${!headers[@]}"
    do
       headersTXT="${headersTXT}
@@ -24,7 +24,7 @@ if [ -n "${10}" ]; then
    done
 fi
 rewritesTXT=""
-if [ -n "${11}" ]; then
+if [ -n "${10}" ]; then
    for element in "${!rewrites[@]}"
    do
       rewritesTXT="${rewritesTXT}
@@ -32,7 +32,7 @@ if [ -n "${11}" ]; then
    done
 fi
 
-if [ "$8" = "true" ]
+if [ "$7" = "true" ]
 then configureXhgui="
 location /xhgui {
         try_files \$uri \$uri/ /xhgui/index.php?\$args;
