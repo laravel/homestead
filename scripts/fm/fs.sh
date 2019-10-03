4c666308c55171cbbd222626fe4925637222f816
Daniel Puente <danny@musicbed.com>
2019-10-03 14:03:03 -0500

    fs setup

--- scripts/fm/fs.sh
+++ scripts/fm/fs.sh
@@ -29,3 +29,32 @@ if [ ! -d ~/Code/musicbed/filmsupply ]; then
     mkdir -p ~/Code/musicbed/filmsupply
 	git clone https://github.com/musicbed/filmsupply.git ~/Code/musicbed/filmsupply
 fi
+
+if [ ! -f ~/Code/musicbed/filmsupply/.env ]; then
+	echo "Adding .env to filmsupply"
+	curl -o ~/Code/musicbed/filmsupply/.env https://s3.amazonaws.com/mb-engineering-onboarding/filmsupply/.env
+	if grep -q "AccessDenied" ~/Code/musicbed/filmsupply/.env; then
+		echo "File not downloaded. Access Denied. Please make sure you are connected to VPN."
+		echo "Cleaning up..."
+		rm -f ~/Code/musicbed/filmsupply/.env
+		exit 1
+	fi
+fi
+
+read -p "Have you ran yarn on filmsupply? y/n" -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+	cd ~/Code/musicbed/filmsupply
+	yarn
+	cd ~/Homestead
+fi
+
+read -p "Have you ran yarn gulp imagemin on filmsupply? y/n" -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+	cd ~/Code/musicbed/musicbed-www
+	yarn gulp imagemin
+	cd ~/Homestead
+fi
