2d5b447a7e169f3274bcab5315435c3bad36b067
Daniel Puente <danny@musicbed.com>
2019-10-03 14:52:07 -0500

    change wording on box add

--- scripts/fm/vagrant.sh
+++ scripts/fm/vagrant.sh
@@ -2,9 +2,9 @@
 
 # Check if vagrant box has been added, if not, prompot user to add
 
-read -p "Run vagrant box add? y/n" -n 1 -r
+read -p "Have you ran vagrant box add laravel/homestead? y/n" -n 1 -r
 echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
 	echo "adding vagrant box laravel/homestead"
 	BRAND=musicbed vagrant box add laravel/homestead
 fi
