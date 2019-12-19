72b1f43663ca038e06a8488cd0b31d1e630aadef
Shaharia Azam <shaharia.azam@gmail.com>
2019-12-20 04:14:02 +0600

    Git repo url updated
    Github URL has been changed for `oh-my-zsh`.

--- scripts/features/ohmyzsh.sh
+++ scripts/features/ohmyzsh.sh
@@ -10,7 +10,7 @@ touch /home/vagrant/.homestead-features/oh-my-zsh
 chown -Rf vagrant:vagrant /home/vagrant/.homestead-features
 
 # Install oh-my-zsh
-git clone https://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
+git clone https://github.com/ohmyzsh/ohmyzsh.git /home/vagrant/.oh-my-zsh
 cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
 printf "\nemulate sh -c 'source ~/.bash_aliases'\n" | tee -a /home/vagrant/.zprofile
 printf "\nemulate sh -c 'source ~/.profile'\n" | tee -a /home/vagrant/.zprofile
