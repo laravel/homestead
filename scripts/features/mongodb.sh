38b90cff8813e4013dfbf81f2545591b9c918f70
Andrew Brown <browner12@gmail.com>
2020-01-17 16:37:12 -0600

    update mongodb to v4.2

--- scripts/features/mongodb.sh
+++ scripts/features/mongodb.sh
@@ -12,7 +12,7 @@ touch /home/vagrant/.homestead-features/mongodb
 chown -Rf vagrant:vagrant /home/vagrant/.homestead-features
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 2>&1
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
 sudo apt-get update
 
