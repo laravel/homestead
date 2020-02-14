05c1e7913ac85f3cc168677f76721eb0c308bbe8
Li Tan <litan1106@gmail.com>
2020-02-14 06:37:35 -0800

    added-masonite-python (#7)
    added masonite python (Laravel inspired web framework for Python 3.5+)

--- scripts/features/python.sh
+++ scripts/features/python.sh
@@ -16,3 +16,4 @@ apt-get update
 apt-get install -y python3-pip build-essential libssl-dev libffi-dev python3-dev python3-venv python-django
 sudo -H -u vagrant bash -c 'pip3 install django'
 sudo -H -u vagrant bash -c 'pip3 install numpy'
+sudo -H -u vagrant bash -c 'pip3 install masonite'
