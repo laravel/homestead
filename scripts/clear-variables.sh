9544ebbd344a666ef77dc0bbee891471820db43b
Joe Ferguson <joe@joeferguson.me>
2020-03-04 14:54:46 -0600

    Update clear variables to be consistent. h/t ProjectCleverWeb

--- scripts/clear-variables.sh
+++ scripts/clear-variables.sh
@@ -3,5 +3,9 @@
 # Clear The Old Environment Variables
 
 sed -i '/# Set Homestead Environment Variable/,+1d' /home/vagrant/.profile
+sed -i '/env\[.*/,+1d' /etc/php/5.6/fpm/pool.d/www.conf
+sed -i '/env\[.*/,+1d' /etc/php/7.0/fpm/pool.d/www.conf
 sed -i '/env\[.*/,+1d' /etc/php/7.1/fpm/pool.d/www.conf
 sed -i '/env\[.*/,+1d' /etc/php/7.2/fpm/pool.d/www.conf
+sed -i '/env\[.*/,+1d' /etc/php/7.3/fpm/pool.d/www.conf
+sed -i '/env\[.*/,+1d' /etc/php/7.4/fpm/pool.d/www.conf
