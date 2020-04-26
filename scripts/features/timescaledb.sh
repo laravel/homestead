aafcd08ea78945e8dbb9eafc9a886c3ddf6451bd
Joe Ferguson <joe@joeferguson.me>
2020-04-26 18:36:02 -0500

    Add timestampDB to each Postgresql Version

--- scripts/features/timescaledb.sh
+++ scripts/features/timescaledb.sh
@@ -12,16 +12,19 @@ touch /home/vagrant/.homestead-features/timescale
 chown -Rf vagrant:vagrant /home/vagrant/.homestead-features
 
 # Add TimeScaleDB PPA
-sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -c -s`-pgdg main' >> /etc/apt/sources.list.d/pgdg.list"
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-
 sudo add-apt-repository -y ppa:timescale/timescaledb-ppa
 sudo apt-get update
 
 # Now install appropriate package for PG version
+sudo apt-get -y install timescaledb-oss-postgresql-9.6
+sudo apt-get -y install timescaledb-oss-postgresql-10
 sudo apt-get -y install timescaledb-oss-postgresql-11
+sudo apt-get -y install timescaledb-oss-postgresql-12
 
 sudo timescaledb-tune --quiet --yes
+printf "\ntimescaledb.telemetry_level=off\n" | sudo tee -a /etc/postgresql/9.6/main/postgresql.conf
+printf "\ntimescaledb.telemetry_level=off\n" | sudo tee -a /etc/postgresql/10/main/postgresql.conf
 printf "\ntimescaledb.telemetry_level=off\n" | sudo tee -a /etc/postgresql/11/main/postgresql.conf
+printf "\ntimescaledb.telemetry_level=off\n" | sudo tee -a /etc/postgresql/12/main/postgresql.conf
 
 sudo service postgresql restart
