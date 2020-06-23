0998e40ec17611ae6f11f555a19fd5f08475ed38
Juan Francisco Giordana <juangiordana@gmail.com>
2020-06-23 11:48:29 -0300

    Add docker-compose support. (#1436)
    Add docker-compose support to docker feature.

--- scripts/features/docker.sh
+++ scripts/features/docker.sh
@@ -14,3 +14,9 @@ curl -fsSL https://get.docker.com | bash -s
 
 # Enable vagrant user to run docker commands
 usermod -aG docker vagrant
+
+# Install docker-compose
+curl \
+    -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" \
+    -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
