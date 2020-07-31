d0322112fb899c60f4206ce8ef2c1adb2b4ba9de
Juan Francisco Giordana <juangiordana@gmail.com>
2020-07-31 07:53:41 -0300

    Features: Python tools script improvements.
    * Begin script with`set -e` to "Exit immediately if a pipeline, a list, or a
    compound command, exits with a non-zero status.".
    
    Commands with successful non-zero exit statuses can be handled separately.
    
    * Remove "Check if installed" comment since code is self explanatory.
    
    * Mark script as done when all commands where executed successfully.

--- scripts/features/python.sh
+++ scripts/features/python.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-# Check If python Has Been Installed
+set -e
 
 if [ -f /home/vagrant/.homestead-features/pythontools ]
 then
-    echo "python already installed."
+    echo "pythontools already installed."
     exit 0
 fi
 
-touch /home/vagrant/.homestead-features/pythontools
-chown -Rf vagrant:vagrant /home/vagrant/.homestead-features
-
-# Install Python
 apt-get update
 apt-get install -y python3-pip build-essential libssl-dev libffi-dev python3-dev python3-venv python3-django
 sudo -H -u vagrant bash -c 'pip3 install django'
 sudo -H -u vagrant bash -c 'pip3 install numpy'
 sudo -H -u vagrant bash -c 'pip3 install masonite'
+
+sudo -g vagrant -u vagrant touch /home/vagrant/.homestead-features/pythontools
