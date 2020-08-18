298fdb9bf24c9a8b788ad5798b030c93ff6d3a87
Joe Ferguson <joe@joeferguson.me>
2020-08-18 17:59:30 -0500

    Update MongoDB for 4..4 / Ubuntu 20.04

--- scripts/features/mongodb.sh
+++ scripts/features/mongodb.sh
@@ -11,9 +11,9 @@ fi
 touch /home/vagrant/.homestead-features/mongodb
 chown -Rf vagrant:vagrant /home/vagrant/.homestead-features
 
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 
 sudo apt-get update
 
