a814534b5cab5e980ed32420e0790efbbb5e2e03
Nick <martianatwork@users.noreply.github.com>
2020-09-08 20:51:37 +0530

    Fix Rabbitmq installation & use https endpoint

--- scripts/features/rabbitmq.sh
+++ scripts/features/rabbitmq.sh
@@ -14,8 +14,8 @@ chown -Rf vagrant:vagrant /home/vagrant/.homestead-features
 # Setup Repositories
 wget -q -O- https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc | sudo apt-key add -
 wget -q -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
-echo "deb http://dl.bintray.com/rabbitmq-erlang/debian bionic erlang" | sudo tee /etc/apt/sources.list.d/rabbitmq.list
-echo "deb https://dl.bintray.com/rabbitmq/debian $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/rabbitmq.list
+echo "deb https://dl.bintray.com/rabbitmq-erlang/debian bionic erlang" | sudo tee /etc/apt/sources.list.d/rabbitmq.list
+echo "deb https://dl.bintray.com/rabbitmq-erlang/debian $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/rabbitmq.list
 sudo apt-get update
 
 # Install RabbitMQ
