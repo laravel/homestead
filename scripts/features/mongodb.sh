e80065a708144e7a813660f3e8165cdefacc405d
Li Tan <litan1106@gmail.com>
2020-12-02 08:45:09 -0800

    Update MongoDB PHP driver for PHP8 support (#1563)
    Update MongoDB PHP driver for PHP8 support

--- scripts/features/mongodb.sh
+++ scripts/features/mongodb.sh
@@ -102,4 +102,14 @@ sudo ln -s /etc/php/7.4/mods-available/mongo.ini /etc/php/7.4/cli/conf.d/20-mong
 sudo ln -s /etc/php/7.4/mods-available/mongo.ini /etc/php/7.4/fpm/conf.d/20-mongo.ini
 sudo service php7.4-fpm restart
 
+phpize8.0
+./configure --with-php-config=/usr/bin/php-config8.0 > /dev/null
+make clean > /dev/null
+make >/dev/null 2>&1
+sudo make install
+sudo bash -c "echo 'extension=mongodb.so' > /etc/php/8.0/mods-available/mongo.ini"
+sudo ln -s /etc/php/8.0/mods-available/mongo.ini /etc/php/8.0/cli/conf.d/20-mongo.ini
+sudo ln -s /etc/php/8.0/mods-available/mongo.ini /etc/php/8.0/fpm/conf.d/20-mongo.ini
+sudo service php8.0-fpm restart
+
 mongo admin --eval "db.createUser({user:'homestead',pwd:'secret',roles:['root']})"
