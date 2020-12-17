8366455e23f4bfbda42e7d7772a7ae2df2865db4
Skyler Katz <skylerkatz@hey.com>
2020-12-17 10:06:42 -0800

    Add php version to cron schedule (#1568)

--- scripts/cron-schedule.sh
+++ scripts/cron-schedule.sh
@@ -6,7 +6,8 @@ fi
 
 SITE_DOMAIN=$1
 SITE_PUBLIC_DIRECTORY=$2
+SITE_PHP_VERSION=$3
 
-cron="* * * * * vagrant  . /home/vagrant/.profile; /usr/bin/php $SITE_PUBLIC_DIRECTORY/../artisan schedule:run >> /dev/null 2>&1"
+cron="* * * * * vagrant  . /home/vagrant/.profile; /usr/bin/php$SITE_PHP_VERSION $SITE_PUBLIC_DIRECTORY/../artisan schedule:run >> /dev/null 2>&1"
 
 echo "$cron" > "/etc/cron.d/$SITE_DOMAIN"
