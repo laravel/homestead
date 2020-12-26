44205811d3b61cfe5bdf5ecdb38b8d9e88f91e17
Joe Ferguson <joe@joeferguson.me>
2020-12-26 13:00:25 -0600

    20.04 dev -> 20.04 (#1581)
    * Snapshot database backup changes
    
    * Fix gitignore for .backup
    
    * Updated MongoDB PHP driver to v1.9.0 (#1562)
    
    Updated MongoDB PHP driver to v1.8.0 with PHP 8.0 support
    
    * Update MongoDB PHP driver for PHP8 support (#1563)
    
    Update MongoDB PHP driver for PHP8 support
    
    * updated xdebug.ini for xdebug 3 support (#1565)
    
    Co-authored-by: Li Tan <litan1106@gmail.com>

--- scripts/features/mongodb.sh
+++ scripts/features/mongodb.sh
@@ -34,7 +34,7 @@ sudo systemctl enable mongod
 sudo systemctl start mongod
 
 sudo rm -rf /tmp/mongo-php-driver /usr/src/mongo-php-driver
-git clone -c advice.detachedHead=false -q -b '1.8.0' --single-branch https://github.com/mongodb/mongo-php-driver.git /tmp/mongo-php-driver
+git clone -c advice.detachedHead=false -q -b '1.9.0' --single-branch https://github.com/mongodb/mongo-php-driver.git /tmp/mongo-php-driver
 sudo mv /tmp/mongo-php-driver /usr/src/mongo-php-driver
 cd /usr/src/mongo-php-driver
 git submodule -q update --init
@@ -101,5 +101,15 @@ sudo bash -c "echo 'extension=mongodb.so' > /etc/php/7.4/mods-available/mongo.in
 sudo ln -s /etc/php/7.4/mods-available/mongo.ini /etc/php/7.4/cli/conf.d/20-mongo.ini
 sudo ln -s /etc/php/7.4/mods-available/mongo.ini /etc/php/7.4/fpm/conf.d/20-mongo.ini
 sudo service php7.4-fpm restart
+
+phpize8.0
+./configure --with-php-config=/usr/bin/php-config8.0 > /dev/null
+make clean > /dev/null
+make >/dev/null 2>&1
+sudo make install
+sudo bash -c "echo 'extension=mongodb.so' > /etc/php/8.0/mods-available/mongo.ini"
+sudo ln -s /etc/php/8.0/mods-available/mongo.ini /etc/php/8.0/cli/conf.d/20-mongo.ini
+sudo ln -s /etc/php/8.0/mods-available/mongo.ini /etc/php/8.0/fpm/conf.d/20-mongo.ini
+sudo service php8.0-fpm restart
 
 mongo admin --eval "db.createUser({user:'homestead',pwd:'secret',roles:['root']})"
