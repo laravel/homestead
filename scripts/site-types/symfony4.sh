889a843095a49ebf7c7d814677c5e805d7933f97
Sébastien Despont <sebastien.despont@gmail.com>
2020-12-26 03:13:05 +0100

    Update symfony4.sh (#1578)
    Add request timeout

--- scripts/site-types/symfony4.sh
+++ scripts/site-types/symfony4.sh
@@ -73,6 +73,9 @@ block="server {
         fastcgi_intercept_errors off;
         fastcgi_buffer_size 16k;
         fastcgi_buffers 4 16k;
+        fastcgi_connect_timeout 300;
+        fastcgi_send_timeout 300;
+        fastcgi_read_timeout 300;
     }
 
     location ~ /\.ht {
