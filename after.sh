950a508a54259a6f39f940c565f6a3918b5c664c
Freddie Bingham <fbingha@gmail.com>
2021-03-02 13:06:22 -0800

    Update after.sh

--- after.sh
+++ after.sh
@@ -32,6 +32,11 @@ line="*/5 * * * * /usr/bin/indexer --config /etc/sphinxsearch/sphinx.conf --rota
 sudo service sphinxsearch start
 #sudo /usr/bin/indexer --config /etc/sphinxsearch/sphinx.conf --rotate --all
 
+# Update PHP memory limit
+sudo sed -i 's/memory_limit = 512M/memory_limit = 4096M/g' /etc/php/7.3/fpm/php.ini
+sudo sed -i 's/memory_limit = 512M/memory_limit = 4096M/g' /etc/php/7.3/cli/php.ini
+sudo service apache2 restart
+
 # This is for CGR Admin which requires mcrypt
 sudo apt-get -y install gcc make autoconf libc-dev pkg-config
 sudo apt-get -y install libmcrypt-dev
