dcff3240b922401d19f0676516b93fcfe6b14895
Li Tan <li.tan@logicalposition.com>
2021-04-21 10:59:30 -0700

    fixed roadrunner path

--- scripts/features/roadrunner.sh
+++ scripts/features/roadrunner.sh
@@ -9,11 +9,8 @@ fi
 touch /home/vagrant/.homestead-features/roadrunner
 chown -Rf vagrant:vagrant /home/vagrant/.homestead-features
 
-mkdir -p /usr/local/roadrunner
-printf "\nPATH=\"/usr/local/roadrunner:\$PATH\"\n" | tee -a /home/vagrant/.profile
-
 # Install Roadrunner CLI
 roadrunnerVersion="2.0.4"
 wget https://github.com/spiral/roadrunner-binary/releases/download/v${roadrunnerVersion}/roadrunner-${roadrunnerVersion}-linux-amd64.tar.gz -qO roadrunner.tar.gz
-tar -xf roadrunner.tar.gz -C /usr/local/roadrunner --strip-components=1
+tar -xf roadrunner.tar.gz -C /usr/local/ --strip-components=1
 rm -rf roadrunner.tar.gz
