24b672a4310b1f38eb82501b9739863189bafaba
Li Tan <li.tan@logicalposition.com>
2021-04-23 15:46:06 -0700

    updated octane.sh

--- scripts/site-types/octane.sh
+++ scripts/site-types/octane.sh
@@ -1,55 +1,91 @@
 #!/usr/bin/env bash
 
 declare -A params=$6       # Create an associative array
-declare -A headers=$9      # Create an associative array
+declare -A headers=${9}    # Create an associative array
+declare -A rewrites=${10}  # Create an associative array
 paramsTXT=""
 if [ -n "$6" ]; then
    for element in "${!params[@]}"
    do
       paramsTXT="${paramsTXT}
-        ${element} ${params[$element]};"
+      fastcgi_param ${element} ${params[$element]};"
    done
 fi
 headersTXT=""
-if [ -n "$9" ]; then
+if [ -n "${9}" ]; then
    for element in "${!headers[@]}"
    do
       headersTXT="${headersTXT}
-        add_header ${element} ${headers[$element]};"
+      add_header ${element} ${headers[$element]};"
+   done
+fi
+rewritesTXT=""
+if [ -n "${10}" ]; then
+   for element in "${!rewrites[@]}"
+   do
+      rewritesTXT="${rewritesTXT}
+      location ~ ${element} { if (!-f \$request_filename) { return 301 ${rewrites[$element]}; } }"
    done
 fi
 
-if [ -n "$2" ]
-then
-    if ! [[ "$2" =~ ^[0-9]+$ ]]
-    then
-        proxyPass="
-        proxy_pass ${2};
-        "
-    else proxyPass="
-        proxy_pass http://127.0.0.1:$2;
-        "
-    fi
-else proxyPass="
-proxy_pass http://127.0.0.1;
+if [ "$7" = "true" ]
+then configureXhgui="
+location /xhgui {
+        try_files \$uri \$uri/ /xhgui/index.php?\$args;
+}
 "
+else configureXhgui=""
 fi
 
-block="server {
+block="upstream swoole-http {
+    server 127.0.0.1:18000;
+}
+
+map \$http_upgrade \$connection_upgrade {
+    default upgrade;
+    ''      close;
+}
+
+server {
     listen ${3:-80};
-    listen ${4:-443} ssl;
+    listen ${4:-443} ssl http2;
     server_name .$1;
+    root \"$2\";
+
+    index index.html index.htm index.php;
+
+    charset utf-8;
+
+    $rewritesTXT
+
+    location = /index.php {
+        try_files /not_exists @swoole;
+    }
 
     location / {
+        try_files \$uri \$uri/ @swoole;
+    }
+
+    location @swoole {
+        set \$suffix '';
+
+        if (\$uri = /index.php) {
+            set \$suffix ?\$query_string;
+        }
+
+        proxy_set_header Scheme \$scheme;
+        proxy_set_header SERVER_PORT \$server_port;
+        proxy_set_header REMOTE_ADDR \$remote_addr;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection \$connection_upgrade;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection "upgrade";
+        proxy_set_header X-Forwarded-Proto \$scheme;
         proxy_set_header Host \$host;
         proxy_http_version 1.1;
-        $proxyPass
+        proxy_pass http://swoole-http\$suffix;
+
         $headersTXT
-        $paramsTXT
     }
 
     location /socket.io {
@@ -74,8 +110,19 @@ block="server {
         proxy_http_version 1.1;
     }
 
+    $configureXhgui
+
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location = /robots.txt  { access_log off; log_not_found off; }
+
     access_log off;
     error_log  /var/log/nginx/$1-error.log error;
+
+    sendfile off;
+
+    location ~ /\.ht {
+        deny all;
+    }
 
     ssl_certificate     /etc/ssl/certs/$1.crt;
     ssl_certificate_key /etc/ssl/certs/$1.key;
