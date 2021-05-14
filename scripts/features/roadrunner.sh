8861d9a8fb02b6a6df657f3b297960b65b38258d
Li Tan <li.tan@logicalposition.com>
2021-05-14 07:42:29 -0700

    bumped roadrunner versino

--- scripts/features/roadrunner.sh
+++ scripts/features/roadrunner.sh
@@ -10,7 +10,7 @@ touch /home/vagrant/.homestead-features/roadrunner
 chown -Rf vagrant:vagrant /home/vagrant/.homestead-features
 
 # Install Roadrunner CLI
-roadrunnerVersion="2.1.1"
+roadrunnerVersion="2.2.1"
 wget https://github.com/spiral/roadrunner-binary/releases/download/v${roadrunnerVersion}/roadrunner-${roadrunnerVersion}-linux-amd64.tar.gz -qO roadrunner.tar.gz
 tar -xf roadrunner.tar.gz -C /usr/local/bin/ --strip-components=1
 rm -rf roadrunner.tar.gz
