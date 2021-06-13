8db526ce99c889973a0607064907e7cb2274862d
Joe Ferguson <joe@joeferguson.me>
2021-06-13 10:57:20 -0500

    Removed PostgreSQL < 13 for WSL & TimescaleDB

--- scripts/features/timescaledb.sh
+++ scripts/features/timescaledb.sh
@@ -22,27 +22,11 @@ touch /home/$WSL_USER_NAME/.homestead-features/timescale
 sudo add-apt-repository -y ppa:timescale/timescaledb-ppa
 sudo apt-get update
 
-# Now install appropriate package for PG version
-if [ -f ~/.homestead-features/wsl_user_name ]; then
-    sudo apt-get -y install timescaledb-2-postgresql-13
-else
-    sudo apt-get -y install timescaledb-postgresql-9.6
-    sudo apt-get -y install timescaledb-postgresql-10
-    sudo apt-get -y install timescaledb-2-postgresql-11
-    sudo apt-get -y install timescaledb-2-postgresql-12
-    sudo apt-get -y install timescaledb-2-postgresql-13
-fi
+sudo apt-get -y install timescaledb-2-postgresql-12
 
 sudo timescaledb-tune --quiet --yes
-if [ -f ~/.homestead-features/wsl_user_name ]; then
-    printf "\ntimescaledb.telemetry_level=off\n" | sudo tee -a /etc/postgresql/13/main/postgresql.conf
-else
-    printf "\ntimescaledb.telemetry_level=off\n" | sudo tee -a /etc/postgresql/9.6/main/postgresql.conf
-    printf "\ntimescaledb.telemetry_level=off\n" | sudo tee -a /etc/postgresql/10/main/postgresql.conf
-    printf "\ntimescaledb.telemetry_level=off\n" | sudo tee -a /etc/postgresql/11/main/postgresql.conf
-    printf "\ntimescaledb.telemetry_level=off\n" | sudo tee -a /etc/postgresql/12/main/postgresql.conf
-    printf "\ntimescaledb.telemetry_level=off\n" | sudo tee -a /etc/postgresql/13/main/postgresql.conf
-fi
+printf "\ntimescaledb.telemetry_level=off\n" | sudo tee -a /etc/postgresql/12/main/postgresql.conf
 
 sudo service postgresql restart
+
 chown -Rf $WSL_USER_NAME:$WSL_USER_GROUP /home/$WSL_USER_NAME/.homestead-features
