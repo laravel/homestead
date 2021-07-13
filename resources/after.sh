ab76761f480a6ab0a0bd74e09dcc4cac86fb9906
Stefano Borghi <stebogit@users.noreply.github.com>
2021-07-13 06:54:32 -0700

    Update after.sh (#1672)
    * Update after.sh
    
    Updates Node version
    
    * Update after.sh

--- resources/after.sh
+++ resources/after.sh
@@ -8,19 +8,16 @@
 # to apply, you may also create user-customizations.sh,
 # which will be run after this script.
 
-# If you're not quite ready for Node 12.x
-# Uncomment these lines to roll back to
-# v11.x or v10.x
 
-# Remove Node.js v12.x:
+# If you're not quite ready for the latest Node.js version,
+# uncomment these lines to roll back to a previous version
+
+# Remove current Node.js version:
 #sudo apt-get -y purge nodejs
 #sudo rm -rf /usr/lib/node_modules/npm/lib
 #sudo rm -rf //etc/apt/sources.list.d/nodesource.list
 
-# Install Node.js v11.x
-#curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-#sudo apt-get install -y nodejs
-
-# Install Node.js v10.x
-#curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+# Install Node.js Version desired (i.e. v13)
+# More info: https://github.com/nodesource/distributions/blob/master/README.md#debinstall
+#curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 #sudo apt-get install -y nodejs
