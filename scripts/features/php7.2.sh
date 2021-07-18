7a8c526c78bf0159a0f375553ab94dcc937f9be9
Joe Ferguson <joe@joeferguson.me>
2021-07-18 17:40:32 -0500

    Adding/Tweaking PHP version scripts

--- scripts/features/php7.2.sh
+++ scripts/features/php7.2.sh
@@ -25,7 +25,7 @@ php7.2-bcmath php7.2-bz2 php7.2-dba php7.2-enchant php7.2-fpm php7.2-imap php7.2
 php7.2-mbstring php7.2-phpdbg php7.2-soap php7.2-sybase php7.2-xsl php7.2-zip php7.2-cgi php7.2-cli php7.2-common \
 php7.2-curl php7.2-dev php7.2-gd php7.2-gmp php7.2-json php7.2-ldap php7.2-mysql php7.2-odbc php7.2-opcache \
 php7.2-pgsql php7.2-pspell php7.2-readline php7.2-recode php7.2-snmp php7.2-sqlite3 php7.2-tidy php7.2-xdebug \
-php7.2-xml php7.2-xmlrpc php7.2-memcached
+php7.2-xml php7.2-xmlrpc php7.2-memcached php7.2-redis
 
 # Configure php.ini for CLI
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.2/cli/php.ini
