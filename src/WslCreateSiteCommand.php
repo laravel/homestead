885da8bca99016a7a428850c189b80978c6f676d
Joe Ferguson <joe@joeferguson.me>
2021-09-11 15:48:11 -0500

    Process headers & rewrites for wsl:create-sites

--- src/WslCreateSiteCommand.php
+++ src/WslCreateSiteCommand.php
@@ -74,21 +74,37 @@ class WslCreateSiteCommand extends Command
         foreach ($settings['wsl_sites'] as $key => $site) {
             $type = $site['type'] ?? 'laravel';
             $create_cmd = '';
+            $headers = false;
+            $rewrites = false;
+            // check & process headers
+            if(array_key_exists('headers', $site)){
+                foreach($site['headers'] as $header) {
+                    $headers[$header['key']] = $header['value'];
+                }
+            }
+            // check & process rewrites
+            if(array_key_exists('rewrites', $site)){
+                foreach($site['rewrites'] as $rewrite) {
+                    $rewrites[$rewrite['map']] = $rewrite['to'];
+                }
+            }
+
             $args = [
-                $site['map'],           // $1
-                $site['to'],            // $2
-                $site['port'] ?? 80,    // $3
-                $site['ssl'] ?? 443,    // $4
-                $site['php'] ?? '7.4',  // $5
-                '',                     // $6 params
-                $site['xhgui'] ?? '',   // $7
-                $site['exec'] ?? false, // $8
-                '',                     // $9 headers
-                '',                     // $10 rewrites
+                $site['map'],           // $0
+                $site['to'],            // $1
+                $site['port'] ?? 80,    // $2
+                $site['ssl'] ?? 443,    // $3
+                $site['php'] ?? '8.0',  // $4
+                '',                     // $5 params
+                $site['xhgui'] ?? '',   // $6
+                $site['exec'] ?? false, // $7
+                $headers,               // $8 headers
+                $rewrites,              // $9 rewrites
             ];
+
             $create_cmd = "sudo bash {$this->basePath}/scripts/site-types/{$type}.sh {$args[0]} \"{$args[1]}\"";
             $create_cmd .= " {$args[2]} {$args[3]} {$args[4]} {$args[5]} {$args[6]} {$args[7]} {$args[8]} {$args[9]}";
-            $create_cmd .= " {$args[10]}";
+
 
             // run command to create the site
             $shell_output = shell_exec($create_cmd);
