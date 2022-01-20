7806f39104749908a8b40a0252894c8d1ce76dba
Li Tan <litan1106@gmail.com>
2022-01-20 11:11:12 -0800

    fixed the roadrunner.sh

--- scripts/features/roadrunner.sh
+++ scripts/features/roadrunner.sh
@@ -24,4 +24,5 @@ chown -Rf $WSL_USER_NAME:$WSL_USER_GROUP /home/$WSL_USER_NAME/.homestead-feature
 wget https://github.com/spiral/roadrunner-binary/releases/download/v${ROADRUNNER_VERSION}/roadrunner-${ROADRUNNER_VERSION}-linux-amd64.tar.gz -qO roadrunner.tar.gz
 tar -xf roadrunner.tar.gz -C /usr/local/bin/ --strip-components=1
 rm -rf roadrunner.tar.gz
+rm -rf /usr/bin/rr
 ln -s /usr/local/bin/rr /usr/bin/rr
