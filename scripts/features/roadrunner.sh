b4feaa21bbeed74f4ae57352dda51d091b203814
Li Tan <litan1106@gmail.com>
2022-01-20 14:07:11 -0800

    bumped roadrunner version to 2.7.2 for octane 1.2.0

--- scripts/features/roadrunner.sh
+++ scripts/features/roadrunner.sh
@@ -9,7 +9,7 @@ else
 fi
 
 export DEBIAN_FRONTEND=noninteractive
-ROADRUNNER_VERSION="2.6.3"
+ROADRUNNER_VERSION="2.7.2"
 
 if [ -f /home/$WSL_USER_NAME/.homestead-features/roadrunner-$ROADRUNNER_VERSION ]
 then
