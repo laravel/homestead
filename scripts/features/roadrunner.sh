c8f14ee24766c2b7ac33299efdc19c865ff3957a
Li Tan <litan1106@gmail.com>
2022-01-20 14:21:56 -0800

    updated roadrunner-grpc.sh

--- scripts/features/roadrunner.sh
+++ scripts/features/roadrunner.sh
@@ -9,7 +9,7 @@ else
 fi
 
 export DEBIAN_FRONTEND=noninteractive
-ROADRUNNER_VERSION="2.7.2"
+ROADRUNNER_VERSION="2.7.3"
 
 if [ -f /home/$WSL_USER_NAME/.homestead-features/roadrunner-$ROADRUNNER_VERSION ]
 then
@@ -21,8 +21,15 @@ touch /home/$WSL_USER_NAME/.homestead-features/roadrunner-$ROADRUNNER_VERSION
 chown -Rf $WSL_USER_NAME:$WSL_USER_GROUP /home/$WSL_USER_NAME/.homestead-features
 
 # Install Roadrunner CLI
-wget https://github.com/spiral/roadrunner-binary/releases/download/v${ROADRUNNER_VERSION}/roadrunner-${ROADRUNNER_VERSION}-linux-amd64.tar.gz -qO roadrunner.tar.gz
+wget https://github.com/roadrunner-server/roadrunner/releases/download/v${ROADRUNNER_VERSION}/roadrunner-${ROADRUNNER_VERSION}-linux-amd64.tar.gz -qO roadrunner.tar.gz
 tar -xf roadrunner.tar.gz -C /usr/local/bin/ --strip-components=1
 rm -rf roadrunner.tar.gz
 rm -rf /usr/bin/rr
 ln -s /usr/local/bin/rr /usr/bin/rr
+
+# Install Roadrunner GRPC
+wget https://github.com/roadrunner-server/roadrunner/releases/download/v${ROADRUNNER_VERSION}/protoc-gen-php-grpc-${ROADRUNNER_VERSION}-linux-amd64.tar.gz -qO protoc-gen-php-grpc.tar.gz
+tar -xf protoc-gen-php-grpc.tar.gz -C /usr/local/bin/ --strip-components=1
+rm -rf protoc-gen-php-grpc.tar.gz
+rm -rf /usr/bin/protoc-gen-php-grpc
+ln -s /usr/local/bin/protoc-gen-php-grpc /usr/bin/protoc-gen-php-grpc
