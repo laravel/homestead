0c65e2e8a5d58a8cfd4cbefae22265e5bd0500af
Li Tan <litan1106@gmail.com>
2022-02-22 16:05:36 -0800

    bumped roadrunner

--- scripts/features/roadrunner.sh
+++ scripts/features/roadrunner.sh
@@ -9,7 +9,7 @@ else
 fi
 
 export DEBIAN_FRONTEND=noninteractive
-ROADRUNNER_VERSION="2.7.3"
+ROADRUNNER_VERSION="2.7.9"
 
 if [ -f /home/$WSL_USER_NAME/.homestead-features/roadrunner-$ROADRUNNER_VERSION ]
 then
