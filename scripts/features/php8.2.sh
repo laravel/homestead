8058cf80cf204f7c3a1fbfb8a41391aab73f46d6
Joe Ferguson <joe@joeferguson.me>
2023-01-04 22:58:24 -0600

    Add extras not in settler

--- scripts/features/php8.2.sh
+++ scripts/features/php8.2.sh
@@ -25,8 +25,7 @@ php8.2 php8.2-bcmath php8.2-bz2 php8.2-cgi php8.2-cli php8.2-common php8.2-curl 
 php8.2-enchant php8.2-fpm php8.2-gd php8.2-gmp php8.2-imap php8.2-interbase php8.2-intl php8.2-ldap \
 php8.2-mbstring php8.2-mysql php8.2-odbc php8.2-opcache php8.2-pgsql php8.2-phpdbg php8.2-pspell php8.2-readline \
 php8.2-snmp php8.2-soap php8.2-sqlite3 php8.2-sybase php8.2-tidy php8.2-xml php8.2-xsl \
-php8.2-zip
-# php8.2-imagick php8.2-memcached php8.2-redis php8.2-xmlrpc php8.2-xdebug
+php8.2-zip php8.2-imagick php8.2-memcached php8.2-redis php8.2-xmlrpc php8.2-xdebug
 
 # Configure php.ini for CLI
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/8.2/cli/php.ini
