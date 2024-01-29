2c649378f4128f615c0f397be064b382982dc1b4
Nicholas C <55490546+ALameLlama@users.noreply.github.com>
2024-01-30 01:42:26 +1100

    feat: include version (#1945)

--- scripts/create-ngrok.sh
+++ scripts/create-ngrok.sh
@@ -4,7 +4,10 @@ PATH_NGROK="/home/vagrant/.config/ngrok"
 PATH_CONFIG="${PATH_NGROK}/ngrok.yml"
 
 # Only create a ngrok config file if there isn't one already there.
-if [ ! -f $PATH_CONFIG ]
-then
-mkdir -p $PATH_NGROK && echo "web_addr: $1:4040" > $PATH_CONFIG
+if [ ! -f $PATH_CONFIG ]; then
+    mkdir -p "$PATH_NGROK"
+    cat > "$PATH_CONFIG" << EOF
+version: 2
+web_addr: $1:4040
+EOF
 fi
